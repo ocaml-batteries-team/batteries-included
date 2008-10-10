50ec12f0bf4bd00a3a14988367ac4fa98a73ece9
yoric <yoric@b9ea8324-641f-426b-95ce-2518f2e44cc5>
2008-10-10 20:46:27 +0000

    More documentation.
    git-svn-id: svn+ssh://svn.forge.ocamlcore.org/svnroot/batteries@139 b9ea8324-641f-426b-95ce-2518f2e44cc5

---
+++ trunk/batteries/examples/shuffle.ml
@@ -0,0 +1,10 @@
+(* Randomly reorder the elements given on the command-line. 
+
+   Usage:
+   ./shuffle 1 2 3 4 5 6 7 8 9
+*)
+
+open Util.Random with self_init ()
+open Data.Mutable, System, IO
+
+let _ = Array.print ~sep:" " ~last:"\n" nwrite stdout (shuffle (args ()))
