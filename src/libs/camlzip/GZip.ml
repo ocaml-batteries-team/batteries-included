40d35f15eb42fd1cafb6a67245a46e2a994d1eac
Stefano Zacchiroli <zack@upsilon.cc>
2008-11-02 15:55:17 +0100

    finalized code skeleton
    now is compiling, implementations are still dummy though

--- src/libs/camlzip/GZip.ml
+++ src/libs/camlzip/GZip.ml
@@ -1,6 +1,11 @@
 
-let compress input =
-  failwith "Not implemented: input"
+open Batteries_core
+
+let uncompress input =
+  failwith "Not implemented: uncompress"
+
+let compress output =
+  failwith "Not implemented: compress"
 
 let gzip_compress ?(level = 6) =
   failwith "Not implemented: gzip_compress"
