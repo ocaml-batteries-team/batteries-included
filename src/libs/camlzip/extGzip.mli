ae51206660f68dfa1044f61b503e4ab4667f9184
Stefano Zacchiroli <zack@upsilon.cc>
2008-11-02 22:15:57 +0100

    add copyright notices

--- src/libs/camlzip/extGzip.mli
+++ src/libs/camlzip/extGzip.mli
@@ -1,3 +1,23 @@
+(* Batteries Included - (De)Compression modules
+ * 
+ * Copyright (C) 2001 Xavier Leroy, projet Cristal, INRIA Rocquencourt
+ *           (C) 2008 Stefano Zacchiroli <zack@upsilon.cc>
+ * 
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version, with the special
+ * exception on linking described in file LICENSE.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+ * USA *)
 
 (** Mimic the part of Camlzip's [Gzip] API about channels, but working
     on Batteries' input/output channels instead of [Pervasives]'s
