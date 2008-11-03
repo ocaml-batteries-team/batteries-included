ac7abc14658150f3065cbe03c54107dcd4996943
Stefano Zacchiroli <zack@upsilon.cc>
2008-11-04 00:08:55 +0100

    module rename ExtGzip -> InnerGZip
    reationale: that module has nothing to do with other Ext* modules, it
    is just a low-level module under gzip integration

--- src/libs/camlzip/gZip.ml
+++ src/libs/camlzip/gZip.ml
@@ -21,19 +21,19 @@
 open Extlib
 
 let uncompress input =
-  let camlzip_in = ExtGzip.open_input input in
+  let camlzip_in = InnerGZip.open_input input in
     IO.create_in
-      ~read:(fun () -> ExtGzip.input_char camlzip_in)
-      ~input:(ExtGzip.input camlzip_in)
-      ~close:(fun () -> ExtGzip.close_in camlzip_in)
+      ~read:(fun () -> InnerGZip.input_char camlzip_in)
+      ~input:(InnerGZip.input camlzip_in)
+      ~close:(fun () -> InnerGZip.close_in camlzip_in)
 
 let gzip_compress ?level output =
-  let camlzip_out = ExtGzip.open_output ?level output in
+  let camlzip_out = InnerGZip.open_output ?level output in
     IO.create_out
-      ~write:(ExtGzip.output_char camlzip_out)
-      ~output:(ExtGzip.output camlzip_out)
-      ~flush:(fun () -> ExtGzip.flush camlzip_out)
-      ~close:(fun () -> ExtGzip.close_out camlzip_out)
+      ~write:(InnerGZip.output_char camlzip_out)
+      ~output:(InnerGZip.output camlzip_out)
+      ~flush:(fun () -> InnerGZip.flush camlzip_out)
+      ~close:(fun () -> InnerGZip.close_out camlzip_out)
 
 let compress output = gzip_compress ?level:None output
 
