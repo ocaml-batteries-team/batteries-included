d722ea673761e628e8ad7212300a621c4eb14a70
David Teller <David.Teller@ens-lyon.org>
2009-01-29 11:17:06 +0100

    Moving examples around.

---
+++ examples/tools/myocamlbuild.ml
@@ -0,0 +1,1 @@
+/home/yoric/batteries/doc/batteries/myocamlbuild.ml
---
+++ examples/tools/shuffle.ml
@@ -0,0 +1,9 @@
+(* Randomly reorder the elements given on the command-line. 
+
+   Usage:
+   ./shuffle 1 2 3 4 5 6 7 8 9
+*)
+
+open Random with self_init ()
+
+let _ = Array.print ~sep:" " ~first:"" ~last:"\n" output_string stdout (shuffle (args ()))
