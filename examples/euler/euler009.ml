5f16f4a621e993d258dbaee021ee3f1b37cb6e0e
Eric Norige <thelema314@gmail.com>
2009-02-04 08:48:55 -0600

    Added: examples/euler/ -- Code solving the euler project problems using batteries

---
+++ examples/euler/euler009.ml
@@ -0,0 +1,17 @@
+let () = 
+  let max_search = 100 in
+  for n = 1 to max_search do
+    for m = n+1 to max_search do
+      let a = 2 * m * n
+      and b = m * m - n * n
+      and c = m * m + n * n
+      in
+      let s = a + b + c in
+      if 1000 mod s = 0 then
+	let m = (1000 / s) in
+	Printf.printf "mult: %d\n" m;
+	Printf.printf "a: %d b: %d c: %d\n" (a*m) (b*m) (c*m);
+	print_int (a * b * c * m * m * m); print_newline ();
+	exit 0
+    done;
+  done
---
+++ examples/euler/euler010.ml
@@ -0,0 +1,18 @@
+open Enum
+
+let max_val = 2_000_000
+let max_test = max_val |> float |> sqrt |> Float.to_int
+
+let () = 
+  let primes = ref (2--max_val) in
+  let s = ref 0 in
+  let rec loop () = 
+    match get !primes with
+      | None -> print_int !s; print_newline ()
+      | Some p -> 
+	  s := !s + p;
+	  if p < max_test then 
+	    primes := !primes // (fun x -> x mod p != 0); (* damn inefficient *)
+	  loop()
+  in
+  loop ()
