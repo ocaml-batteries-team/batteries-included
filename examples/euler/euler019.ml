216e52d4e143829a7a992a8b0e3fc7105f861db0
Eric Norige <thelema314@gmail.com>
2009-02-14 12:11:14 -0600

    More projecteuler.net solutions

---
+++ examples/euler/euler019.ml
@@ -0,0 +1,50 @@
+let daysmonth = [ 31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31 ]
+let daysleap = [ 31; 29; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31 ]
+
+let year_shift_f days = (List.reduce (+) days) mod 7
+
+let year_shift     = year_shift_f daysmonth (* 1 *)
+let year_shiftleap = year_shift_f daysleap  (* 2 *)
+
+let count_shift days =
+  let ret = Array.make 7 0 in
+  let rec loop day = function
+      h :: [] ->
+	let sh = 6 - day in
+	ret.(sh) <- ret.(sh) + 1 (* and done *)
+    | h :: t  ->
+	let sh = 6 - day in
+	ret.(sh) <- ret.(sh) + 1;
+	loop ((day + h) mod 7) t
+  in
+  loop 0 days;
+  ret
+
+let count_year = count_shift daysmonth
+let count_leap = count_shift daysleap
+(* val count_year : int array = [|2; 2; 1; 3; 1; 1; 2|] *)
+(* val count_leap : int array = [|2; 1; 2; 2; 1; 1; 3|] *)
+
+let is_leap yr =
+  if yr mod 4 <> 0 then false
+  else if yr mod 100 <> 0 then true
+  else if yr mod 400 <> 0 then false
+  else true
+
+let rec count_sun (count, yr, endyr, dayone) =
+  if yr >= endyr then count
+  else
+    let add, shift =
+      if is_leap yr
+      then count_leap.(dayone), year_shiftleap
+      else count_year.(dayone), year_shift
+    in
+    count_sun ((count+add), (yr+1), endyr, ((dayone + shift) mod 7))
+
+let () =
+  let end_yr = 2001
+  and start_yr = 1901
+  and dayone = 1 (* monday *)
+  in
+  let count = count_sun (0, start_yr, end_yr, dayone) in
+  print_int count; print_newline();;
---
+++ examples/euler/euler021.ml
@@ -0,0 +1,43 @@
+let d n =
+  let sum = ref 1 in (* start having counted 1 *)
+  let max_test = n |> float |> sqrt |> Float.to_int in
+  for i = 2 to max_test do
+    if n mod i = 0 then sum := !sum + i + (n / i)
+  done;
+  !sum
+
+module ISet = Set.Make(Int)
+
+let ret_amicable ~upto =
+  let is_amic = ref ISet.empty
+  and not_amic = ref ISet.empty
+  and to_test = ref ((2--upto) |> ISet.of_enum)
+  in
+  let rec test n =
+    if n >= upto then ()
+    else if   ISet.mem n !is_amic
+      || ISet.mem n !not_amic then
+	()
+    else
+      let dn = d n in
+      if dn >= upto || dn = n then
+	not_amic := !not_amic |> ISet.add n
+      else
+	let ddn = d dn in
+	if n = ddn then
+	  is_amic := !is_amic |> ISet.add n |> ISet.add dn
+	else
+	  not_amic := !not_amic |> ISet.add n
+  in
+  while not (ISet.is_empty !to_test) do
+    let n = ISet.choose !to_test in
+    to_test := !to_test |> ISet.remove n;
+    test n
+  done;
+  ISet.enum !is_amic
+
+let print_int_enum e = Enum.print (fun stdout n -> IO.nwrite stdout (string_of_int n)) stdout e
+
+let () =
+  ret_amicable ~upto:10_000 |> Enum.reduce (+) |> print_int;
+  print_newline();;
