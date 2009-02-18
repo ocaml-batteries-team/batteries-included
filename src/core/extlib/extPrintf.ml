59caaadb649bc7ee9ba863d3ec16ec3ff426297f
David Teller <David.Teller@ens-lyon.org>
2009-02-18 18:44:41 +0100

    Fixed PA_TYPE_CONV.

--- src/core/extlib/extPrintf.ml
+++ src/core/extlib/extPrintf.ml
@@ -18,7 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-TYPE_CONV_PATH "Batteries.Languages" (*For Sexplib, Bin-prot...*)
+TYPE_CONV_PATH "" (*For Sexplib, Bin-prot...*)
 
 module Printf = InnerIO.Printf
 
