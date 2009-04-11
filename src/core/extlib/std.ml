c6c1be51fdf359b88ceea07721a6c48fa7202b4f
Eric Norige <thelema314@gmail.com>
2009-04-11 13:57:45 -0500

    Std.ml: comment [invisible_args]

--- src/core/extlib/std.ml
+++ src/core/extlib/std.ml
@@ -219,6 +219,8 @@ let uncurry f (x,y) = f x y
 let const x _ = x
 
 let invisible_args = ref 1
+(* the number or arguments to ignore at the beginning of Sys.argv,
+usually because program-name is put in argv.(0) *)
 
 let args () =
   let e = ExtArray.Array.enum Sys.argv in
