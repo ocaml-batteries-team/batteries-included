3983bfe9e180d098c039ef928ace1495ea8e1912
Mauricio Fernandez <mfp@acm.org>
2009-04-19 20:01:44 +0200

    Restored Compilers testing in test_toplevel.

--- testsuite/test_toplevel.ml
+++ testsuite/test_toplevel.ml
@@ -2,7 +2,7 @@ open OUnit
 
 (*Source code which needs to be executed*)
 
-let make_temporary_file content = 
+let make_temporary_file content =
   File.with_temporary_out ~suffix:".ml"
     begin
       fun out name ->
@@ -15,37 +15,26 @@ let expected = "read-only string";;
 open Compilers
 open IO
 
-(*
-let test_1 =
-  ("OCaml: Testing from source file", fun () ->
-     try
-       let source    = "Print.printf p\"%sc\" ro\"read-only string\";;" in
-       let generated_file = make_temporary_file source                in
-       let temp_name = Filename.temp_file "ocaml" "test"              in
-	 ignore (Sys.command (string_of_command (ocaml [generated_file]) ^ " > " ^ temp_name));
-	 let obtained = File.with_file_in temp_name read_all
-	 in
-	   if obtained = expected then Testing.Pass
-	   else Testing.Fail (Printf.sprintf "Expected: %S\n\tObtained: %S\n" expected obtained)
-     with e -> Testing.Err (Printexc.to_string e))
+let test_from_source_file () =
+  let source    = "Print.printf p\"%sc\" ro\"read-only string\";;" in
+  let generated_file = make_temporary_file source in
+  let temp_name = Filename.temp_file "ocaml" "test" in
+    ignore (Sys.command (string_of_command (ocaml [generated_file]) ^ " > " ^ temp_name));
+    let obtained = File.with_file_in temp_name read_all in
+      assert_equal ~printer:(Printf.sprintf "%S") expected obtained
 
-let test_2 =
-  ("OCaml: Testing from simulated command-line", fun () ->
-     try
-       let temp_name = Filename.temp_file "ocaml" "test" in
-       let source    = Print.sprintf
-	 p"File.with_file_out %S (fun out -> Print.fprintf out p\"%%sc\" ro\"read-only string\");;\n" 
-	 temp_name in
-       let generated_file = make_temporary_file source                in
-       let command   = string_of_command (ocaml []) ^ " < " ^ generated_file  ^ " > /dev/null " in
-(*	 Printf.eprintf "Running %S\nWriting to file %S\n%!" command temp_name;*)
-	 ignore (Sys.command command);
-	 flush_all ();
-	 let obtained  = File.with_file_in temp_name read_all in
-	   if obtained = expected then Testing.Pass
-	   else Testing.Fail (Printf.sprintf "Expected: %S\n\tObtained: %S\n" expected obtained)
-     with e -> Testing.Err (Printexc.to_string e))
- *)
+let test_from_simulated_cmdline () =
+  let temp_name = Filename.temp_file "ocaml" "test" in
+  let source    = Print.sprintf
+                    p"File.with_file_out %S (fun out -> Print.fprintf out p\"%%sc\" ro\"read-only string\");;\n"
+                    temp_name in
+  let generated_file = make_temporary_file source in
+  let command   = string_of_command (ocaml []) ^ " < " ^ generated_file  ^ " > /dev/null " in
+    (*	 Printf.eprintf "Running %S\nWriting to file %S\n%!" command temp_name;*)
+    ignore (Sys.command command);
+    flush_all ();
+    let obtained  = File.with_file_in temp_name read_all in
+      assert_equal ~printer:(Printf.sprintf "%S") expected obtained
 
 (*
 let test_1 =
@@ -66,7 +55,7 @@ let test_1 =
 	   if obtained = expected then Testing.Pass
 	   else Testing.Fail (Printf.sprintf "Expected: %S\n\tObtained: %S\n" expected obtained)
      with e -> Testing.Err (Printexc.to_string e))
-       
+
 let test_2 =
   ("OCaml: Testing from simulated command-line", fun () ->
      try
@@ -78,9 +67,10 @@ let test_2 =
 	   if obtained = expected then Testing.Pass
 	   else Testing.Fail (Printf.sprintf "Expected: %S\n\tObtained: %S\n" expected obtained)
      with e -> Testing.Err (Printexc.to_string e))
-       
+
 *)
 
 let tests = "Toplevel" >::: [
-
+  "From source file" >:: test_from_source_file;
+  "From simulated command-line" >:: test_from_simulated_cmdline;
 ]
