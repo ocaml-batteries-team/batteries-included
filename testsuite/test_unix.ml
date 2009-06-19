1fb0333cb9193dc37107f01419fed70b5d4c658b
David Teller <David.Teller@ens-lyon.org>
2009-06-19 18:55:10 +0200

    Added more complete check for bug #227. Oh, and it's not a bug, by the way.

--- testsuite/test_unix.ml
+++ testsuite/test_unix.ml
@@ -1,9 +1,10 @@
 open OUnit
 open Print
+open IO
 
 let string = "hello world"
 
-let test_open_process () =
+let test_open_process_readline () =
   try
     let r,w = Unix.open_process "cat" in
       fprintf w p"%s\n" string;
@@ -13,8 +14,40 @@ let test_open_process () =
 	| s                 -> assert_failure (sprintf p"Expected %S, got %S" string s)
   with e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)
 
+(*let test_open_process_legacy_readchar () =
+  try
+    let r,w = Unix.open_process ~cleanup:true "cat" in
+      fprintf w p"%s\n" string;
+      close_out w;
+(*      let fdw = Unix.descr_of_output w              in
+	Unix.close fdw;*)(*For some reason, when reading char-by-char
+			   instead of line-by-line, [w] must be closed
+			   with low-level closing*)
+      let fdr = Unix.descr_of_input r               in
+      let ic  = Legacy.Unix.in_channel_of_descr fdr in
+	while true do
+	  ignore (Legacy.Pervasives.input_char ic)
+	done
+  with End_of_file 
+    | No_more_input -> () 
+    | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)*)
+
+let test_open_process_readchar () =
+  try
+    let r,w = Unix.open_process ~cleanup:true "cat" in
+      fprintf w p"%s\n" string;                       
+      close_out w;
+      while true do
+	ignore (input_char r); (*This is a way of checking that the process is closed.*)
+      done
+  with End_of_file 
+    | No_more_input -> () 
+    | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)
+    
+
 
 
 let tests = "Unix" >::: [
-  "Open process" >:: test_open_process
+  "Open process, then read_line" >:: test_open_process_readline;
+  "Open process, then read chars" >:: test_open_process_readchar
 ]
