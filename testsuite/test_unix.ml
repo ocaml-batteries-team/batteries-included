38edf9497ee7ae1a2c01c12051ec84050a01ed2f
David Teller <David.Teller@ens-lyon.org>
2009-06-21 01:04:22 +0200

    Removed erroneous test from testsuite.

--- testsuite/test_unix.ml
+++ testsuite/test_unix.ml
@@ -32,22 +32,31 @@ let test_open_process_readline () =
     | No_more_input -> () 
     | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)*)
 
-let test_open_process_readchar () =
+let test_open_process_cleanup () =
   try
     let r,w = Unix.open_process ~cleanup:true "cat" in
       fprintf w p"%s\n" string;                       
       close_out w;
       while true do
-	ignore (input_char r); (*This is a way of checking that the process is closed.*)
+	ignore (input_char r) (*This is a way of checking that the process is closed.*)
       done
   with End_of_file 
     | No_more_input -> () 
     | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)
-    
 
-
+(*let test_open_process_close_process () = (*Actually, this test shouldn't work*)
+  try
+    let r,w = Unix.open_process  "cat" in
+      fprintf w p"%s\n" string;                       
+      ignore (Unix.close_process (r, w));
+      while true do
+	ignore (input_char r); (*This is a way of checking that the process is closed.*)
+      done
+  with End_of_file 
+    | No_more_input -> () 
+    | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)*)
 
 let tests = "Unix" >::: [
-  "Open process, then read_line" >:: test_open_process_readline;
-  "Open process, then read chars" >:: test_open_process_readchar
+  "Open process, then read_line"     >:: test_open_process_readline;
+  "Open process, then clean up"      >:: test_open_process_cleanup
 ]
