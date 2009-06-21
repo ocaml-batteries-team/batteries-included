7125e92c46079ca2f1c081e89a33cfa99fe0d8ae
David Teller <David.Teller@ens-lyon.org>
2009-06-21 17:45:19 +0200

    Testsuite updated to reflect new behavior of ExtUnix.

--- testsuite/test_unix.ml
+++ testsuite/test_unix.ml
@@ -14,27 +14,9 @@ let test_open_process_readline () =
 	| s                 -> assert_failure (sprintf p"Expected %S, got %S" string s)
   with e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)
 
-(*let test_open_process_legacy_readchar () =
-  try
-    let r,w = Unix.open_process ~cleanup:true "cat" in
-      fprintf w p"%s\n" string;
-      close_out w;
-(*      let fdw = Unix.descr_of_output w              in
-	Unix.close fdw;*)(*For some reason, when reading char-by-char
-			   instead of line-by-line, [w] must be closed
-			   with low-level closing*)
-      let fdr = Unix.descr_of_input r               in
-      let ic  = Legacy.Unix.in_channel_of_descr fdr in
-	while true do
-	  ignore (Legacy.Pervasives.input_char ic)
-	done
-  with End_of_file 
-    | No_more_input -> () 
-    | e -> assert_failure (sprintf p"Expected %S, got exception %exn" string e)*)
-
 let test_open_process_cleanup () =
   try
-    let r,w = Unix.open_process ~cleanup:true "cat" in
+    let r,w = Unix.open_process "cat" in
       fprintf w p"%s\n" string;                       
       close_out w;
       while true do
