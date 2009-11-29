72ab418b2a96ea5d3c9b99310e5209b2fe2e5a77
Eric Norige <thelema314@gmail.com>
2009-11-29 10:18:34 -0500

    Add last level of indirection to extlibcompat

--- src/extlibcompat.ml
+++ src/extlibcompat.ml
@@ -1,4 +1,12 @@
-module Array = struct include Array include BatArray end
-module Hashtbl = BatHashtbl
-module List = struct include List include BatList end
-module String = struct include String include BatString end
+module ExtArray = struct 
+  module Array = struct include Array include BatArray end 
+end
+module ExtHashtbl = struct 
+  module Hashtbl = BatHashtbl 
+end
+module ExtList = struct
+  module List = struct include List include BatList end
+end
+module ExtString = struct
+  module String = struct include String include BatString end
+end
