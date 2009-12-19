d6dc9e036fec362a6bd276eea6fe17c04c80bdc9
Eric Norige <thelema314@gmail.com>
2009-12-19 13:17:10 -0600

    Remove Extlib submodule from batteries.ml

--- src/batteries.ml
+++ src/batteries.ml
@@ -43,21 +43,6 @@ module Legacy = struct
   module Weak = Weak
 end
 
-module Extlib = struct
-  module ExtArray = struct 
-    module Array = struct include Array include BatArray end 
-  end
-  module ExtHashtbl = struct 
-    module Hashtbl = BatHashtbl 
-  end
-  module ExtList = struct
-    module List = struct include List include BatList end
-  end
-  module ExtString = struct
-    module String = struct include String include BatString end
-  end
-end
-
 (* stdlib modules *)
 module Arg = struct include Arg include BatArg end
 module Array = struct include Array include BatArray end
