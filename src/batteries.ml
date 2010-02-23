a7174adc9128fd89e627aaadacb415190479965b
Michael Ekstrand <michael@elehack.net>
2010-02-22 19:02:57 -0600

    Use `RMutex` for locks (references #46).
    There seem to be some lock re-entrancy bugs in `BatIO` that cause
    hangs when we use a plain mutex for synchronization.  Using RMutex
    until those problems are fixed or we determine that they are inherent.

--- src/batteries.ml
+++ src/batteries.ml
@@ -2,7 +2,7 @@ include Batteries_uni (* the meat of this module is in batteries_uni.mli *)
 module Mutex = BatMutex;;
 module RMutex = BatRMutex;;
 
-Unix.lock := Mutex.Mutex.make ();;
-IO.lock := Mutex.Mutex.make ();;
-IO.lock_factory := Mutex.Mutex.make;;
-lock := Mutex.Mutex.make ();;
+Unix.lock := RMutex.make ();;
+IO.lock := RMutex.make ();;
+IO.lock_factory := RMutex.make;;
+lock := RMutex.make ();;
