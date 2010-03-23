7becf684d22ab6e2021c496fef8291c1b6156d93
Eric Norige <thelema314@gmail.com>
2010-03-22 22:00:31 -0400

    commit the myocamlbuild I use to compile batteries projects

--- build/myocamlbuild.ml
+++ build/myocamlbuild.ml
@@ -64,7 +64,7 @@ struct
          flag ["ocaml"; "ocamldep"; "pkg_"^pkg] & S[A"-package"; A pkg];
          flag ["ocaml"; "doc";      "pkg_"^pkg] & S[A"-package"; A pkg];
          flag ["ocaml"; "link";     "pkg_"^pkg] & S[A"-package"; A pkg];
-         flag ["ocaml"; "infer_interface"; "pkg_"^pkg] & S[A"-package"; A pkg];
+	 flag ["ocaml"; "infer_interface"; "pkg_"^pkg] & S[A"-package"; A pkg];
        end (find_packages ());
 
        (* Like -package but for extensions syntax. Morover -syntax is useless
@@ -73,7 +73,7 @@ struct
          flag ["ocaml"; "compile";  "syntax_"^syntax] & S[A"-syntax"; A syntax];
          flag ["ocaml"; "ocamldep"; "syntax_"^syntax] & S[A"-syntax"; A syntax];
          flag ["ocaml"; "doc";      "syntax_"^syntax] & S[A"-syntax"; A syntax];
-         flag ["ocaml"; "infer_interface"; "syntax_"^syntax] & S[A"-syntax"; A syntax];
+         flag ["ocaml"; "infer_interface";  "syntax_"^syntax] & S[A"-syntax"; A syntax];
        end (find_syntaxes ());
 
        (* The default "thread" tag is not compatible with ocamlfind.
@@ -86,22 +86,8 @@ struct
         *)
        flag ["ocaml"; "pkg_threads"; "compile"]  (S[A "-thread"]);
        flag ["ocaml"; "pkg_threads"; "link"]     (S[A "-thread"]);
+       flag ["ocaml"; "pkg_threads"; "infer_interface"] (S[A "-thread"])
 end
-
-(**
-   {1 Utilities}
-*)
-
-(**
-   Determine if a pathname must be compiled as threaded.
-
-   A pathname is considered as threaded if it has tag ["pkg_threads"].
-*)
-let is_threaded name =
-(*  Tags.print Format.std_formatter (tags_of_pathname name);*)
-  let result = Tags.does_match (tags_of_pathname name) (Tags.of_list ["pkg_threads"]) in
-    Printf.eprintf "file %s is %s\n" name (if result then "threaded" else "non-threaded");
-    result
 
 (**
    {1 OCaml Batteries Included}
@@ -119,17 +105,13 @@ struct
     flag ["ocaml"; "doc";            "use_ocamldoc_info"] (S[A "-I"; A "+ocamldoc"]);
 
     (*The command-line for [use_batteries] and [use_batteries_r]*)
-    let cl_use_boilerplate =
-      [A"-package"; A "batteries.pa_type_conv.syntax,batteries,sexplib.syntax"]
-    and cl_use_batteries   =
-      let syntaxes = ["pa_openin";"pa_where";"pa_batteries";"pa_comprehension";"pa_strings"] in
-      [A "-package";
-       A (String.concat "," (List.map (Printf.sprintf "batteries.%s.syntax") syntaxes));
-       A "-package"; A "batteries"]
+
+    let cl_use_boilerplate = [A"-package"; A "batteries.pa_type_conv.syntax,batteries,sexplib.syntax"]
+    and cl_use_batteries   = [A"-package"; A "batteries.pa_openin.syntax,batteries.pa_where.syntax,batteries.pa_batteries.syntax"; A "-package"; A "batteries"]
     and cl_use_batteries_o = []
               (*[cl_use_batteries_o]: extensions which only make sense in original syntax*)
     and cl_camlp4o         = [A"-syntax";  A "camlp4o"]
-    and cl_camlp4r         = [A"-syntax";  A "camlp4r"] in (*Temporary fix -- ocamlfind really behaves strangely*)
+    and cl_camlp4r         = [A"-syntax";  A "camlp4r"] in
 
     let cl_boilerplate_original = cl_use_boilerplate @ cl_camlp4o
     and cl_boilerplate_revised  = cl_use_boilerplate @ cl_camlp4r
@@ -143,7 +125,6 @@ struct
     flag ["ocaml"; "ocamldep"; "use_boilerplate"] & S cl_boilerplate_original ;
     flag ["ocaml"; "doc";      "use_boilerplate"] & S cl_boilerplate_original ;
     flag ["ocaml"; "link";     "use_boilerplate"] & S cl_boilerplate_original ;
-    flag ["ocaml"; "infer_interface"; "use_boilerplate"] & S cl_boilerplate_original;
 
       (** Tag [use_boilerplate_r] provides boilerplate syntax extensions,
 	  in original syntax*)
@@ -152,7 +133,6 @@ struct
     flag ["ocaml"; "ocamldep"; "use_boilerplate_r"] & S cl_boilerplate_revised ;
     flag ["ocaml"; "doc";      "use_boilerplate_r"] & S cl_boilerplate_revised ;
     flag ["ocaml"; "link";     "use_boilerplate_r"] & S cl_boilerplate_revised ;
-    flag ["ocaml"; "infer_interface"; "use_boilerplate_r"] & S cl_boilerplate_revised;
 
     (** Tag [use_batteries] provides both package [batteries]
 	and all syntax extensions, in original syntax. *)
@@ -161,7 +141,6 @@ struct
     flag ["ocaml"; "ocamldep"; "use_batteries"] & S cl_batteries_original ;
     flag ["ocaml"; "doc";      "use_batteries"] & S cl_batteries_original ;
     flag ["ocaml"; "link";     "use_batteries"] & S cl_batteries_original ;
-    flag ["ocaml"; "infer_interface"; "use_batteries"] & S cl_batteries_original;
 
     (** Tag [use_batteries_r] provides both package [batteries]
 	and all syntax extensions, in revised syntax. *)
@@ -169,8 +148,7 @@ struct
     flag ["ocaml"; "compile";  "use_batteries_r"] & S cl_batteries_revised;
     flag ["ocaml"; "ocamldep"; "use_batteries_r"] & S cl_batteries_revised;
     flag ["ocaml"; "doc";      "use_batteries_r"] & S cl_batteries_revised;
-    flag ["ocaml"; "link";     "use_batteries_r"] & S cl_batteries_revised;
-    flag ["ocaml"; "infer_interface"; "use_batteries_r"] & S cl_batteries_revised;
+    flag ["ocaml"; "link";     "use_batteries_r"] & S cl_batteries_revised
 
 
 (*    flag ["ocaml"; "compile";  "use_batteries"] & S[A "-verbose";
@@ -189,141 +167,31 @@ struct
 
 end
 
-
-(**
-   {1 Using Batteries as a dynamically-linked library}
-
-   Due to the model of OCaml, to do this, we actually invert the usual dynamic linking
-   mechanism: Batteries is used as a main program (run.byte/run.native), while the
-   program compiled by the user is actually compiled to a dynamically-loaded .cma/.cmxs.
-*)
-module Dynamic = struct
-
-  (**
-     Produce the name of the runner, depending on whether the code is threaded
-  *)
-  let decide_runner source runner =
-    let dir = if is_threaded source then "@batteries_threads"
-              else                       "@batteries_nothreads"
-    in
-    Printf.sprintf "%s/%s" dir runner
-
-  let generate_stub runner bin =
-    let init_loader =
-      Printf.sprintf
-	 "(*Locate Batteries loader*)
-          Findlib.init ();;
-          let binary = Findlib.resolve_path %S;;"
-	 runner
-    and cl_loader =
-(*(Pathname.concat Pathname.current_dir_name cma)(*Should do one of the following:
-						 - replace this with the complete path
-						 - build the path at run-time from [Sys.argv.(0)]
-						 - find a way to embed the plug-in inside the .byte *)*)
-      Printf.sprintf
-	"(*Prepare command-line*)
-         let buf = Buffer.create 80;;
-         Printf.bprintf buf %S binary (Filename.concat (Filename.dirname (Sys.argv.(0))) %S);;"
-	"%S %S -- "
-	bin
-    and start =
-        "for i = 1 to Array.length Sys.argv - 1 do
-           Printf.bprintf buf \"%S \" Sys.argv.(i)
-         done;;
-        let command = Buffer.contents buf in
-        Printf.eprintf \"Requesting load of %S\\n...\\n%!\" command;
-        Sys.command command
-        "
-    in Printf.sprintf "%s\n%s\n%s\n" init_loader cl_loader start
-
+module Ocamlviz = struct
 
   let before_options () = ()
+
   let after_rules () =
-    begin
-      rule ".cma to _dyn.ml (no threads)"
-	~prod:"%_dynbyte.ml"
-	~dep:"%.cma"
-	begin fun env build ->
-	  let dest   = env "%_dynbyte.ml"
-	  and bin    = env "%.cma"
-          and runner = decide_runner (env "%.dynbyte") "run.byte" in
-	  let contents = generate_stub runner bin
-	  in
-	    tag_file dest ["pkg_findlib"]; (*Magically depend on findlib, without requiring user-intervention in _tags*)
-	    tag_file (env "%_dynbyte.cmo")  ["pkg_findlib"];
-	    tag_file (env "%_dynbyte.byte") ["pkg_findlib"];
-	    Echo ([contents], dest)
-	end;
+    flag ["ocaml"; "compile"; "ocamlviz_auto"] (S[A"-ppopt"; A"camlp4 pa_o.cmo str.cma pa_ocamlviz.cmo pr_o.cmo"]);
+    flag ["ocaml"; "ocamldep"; "ocamlviz_auto"] (S[A"-ppopt"; A"camlp4 pa_o.cmo str.cma pa_ocamlviz.cmo pr_o.cmo"]);
+    flag ["ocaml"; "compile"; "byte"; "ocamlviz"] (S[A"libocamlviz.cma"]);
+    flag ["ocaml"; "compile"; "native"; "ocamlviz"] (S[A"libocamlviz.cmxa"]);
+    flag ["ocaml"; "link"; "byte"; "ocamlviz"] (S[A"libocamlviz.cma"]);
+    flag ["ocaml"; "link"; "native"; "ocamlviz"] (S[A"libocamlviz.cmxa"]);
 
-      rule "_dynbyte.ml to .dynbyte"
-	~prod:"%.dynbyte"
-	~dep: "%_dynbyte.byte"
-	begin
-	  fun env build ->
-	  let dest = env "%.dynbyte"
-	  and src  = env "%_dynbyte.byte"
-	  in
-	    tag_file dest ["pkg_findlib"];
-	    Seq [Cmd (S[A"cp"; A src;  A dest]);
-		 Cmd (S[A"ln"; A"-sf"; P (!Options.build_dir/dest); A Pathname.pwd]
-		     )]
-	end;
-
-      rule ".cmx to cmxs"
-	~prod:"%.cmxs"
-	~dep:"%.cmx"
-	begin fun env build ->
-	  let dest = env "%.cmxs"
-	  and src  = env "%.cmx" in
-	  Cmd (S[A"ocamlopt"; A src; A "-shared"; A "-o"; A dest])
-	end;
-
-      rule ".cmxs to _dynnative.ml (no threads)"
-	~prod:"%_dynnative.ml"
-	~dep:"%.cmxs"
-	begin fun env build ->
-	  let dest   = env "%_dynnative.ml"
-	  and bin    = env "%.cmxs"
-          and runner = decide_runner (env "%.dynnative") "run.native" in
-	  let contents = generate_stub runner bin
-	  in
-	    tag_file dest ["pkg_findlib"]; (*Magically depend on findlib, without requiring user-intervention in _tags*)
-	    tag_file (env "%_dynnative.cmx")    ["pkg_findlib"];
-	    tag_file (env "%_dynnative.native") ["pkg_findlib"];
-	    Echo ([contents], dest)
-	end;
-
-      rule "_dynnative.ml to .dynnative"
-	~prod:"%.dynnative"
-	~dep: "%_dynnative.native"
-	begin
-	  fun env build ->
-	  let dest = env "%.dynnative"
-	  and src  = env "%_dynnative.native"
-	  in
-	    tag_file dest ["pkg_findlib"];
-	    Seq [Cmd (S[A"cp"; A src;  A dest]);
-		 Cmd (S[A"ln"; A"-sf"; P (!Options.build_dir/dest); A Pathname.pwd]
-		     )]
-	end
-
-    end
 end
 
 let _ = dispatch begin function
-   | Before_options ->
-       OCamlFind.before_options ();
-       Batteries.before_options ();
-       Dynamic.before_options ()
-   | After_rules ->
-       OCamlFind.after_rules ();
-       Batteries.after_rules ();
-       Dynamic.after_rules()
-
-
-   | _ -> ()
+  | Before_options ->
+      OCamlFind.before_options ();
+      Batteries.before_options ();
+      Ocamlviz.before_options ();
+  | After_rules ->
+      OCamlFind.after_rules ();
+      Batteries.after_rules ();
+      Ocamlviz.after_rules ()
+  | _ -> ()
 end
-
 
 
 (**
