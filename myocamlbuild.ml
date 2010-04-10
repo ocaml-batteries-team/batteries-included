e517e656921cc1aab2ff57e698dd953931be363f
Michael Ekstrand <michael@elehack.net>
2010-04-10 17:37:30 -0500

    Implement initial ocamlbuild support
    Commits the initial ocamlbuild plugin and control files to allow
    Batteries to be built with ocamlbuild.

---
+++ myocamlbuild.ml
@@ -0,0 +1,31 @@
+(* ocamlbuild plugin for building Batteries.  
+ * Copyright (C) 2010 Michael Ekstrand
+ * 
+ * Portions (hopefully trivial) from build/myocamlbuild.ml and the
+ * Gallium wiki. *)
+
+open Ocamlbuild_plugin
+
+let ocamlfind x = S[A"ocamlfind"; A x]
+
+let packs = String.concat "," ["camomile"; "num"; "str"]
+
+let _ = dispatch begin function
+  | Before_options ->
+      (* Set up to use ocamlfind *)
+      Options.ocamlc     := ocamlfind "ocamlc";
+      Options.ocamlopt   := ocamlfind "ocamlopt";
+      Options.ocamldep   := ocamlfind "ocamldep";
+      Options.ocamldoc   := ocamlfind "ocamldoc";
+      Options.ocamlmktop := ocamlfind "ocamlmktop"
+  | After_rules ->
+      flag ["ocaml"; "compile"] & S[A"-package"; A packs];
+      flag ["ocaml"; "ocamldep"] & S[A"-package"; A packs];
+      flag ["ocaml"; "doc"] & S[A"-package"; A packs];
+      flag ["ocaml"; "link"] & S[A"-package"; A packs];
+      flag ["ocaml"; "infer_interface"] & S[A"-package"; A packs];
+
+      flag ["ocaml"; "compile"; "threads"] & A"-thread";
+      flag ["ocaml"; "link"; "threads"] & A"-thread";
+  | _ -> ()
+end
