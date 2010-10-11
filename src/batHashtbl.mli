13b5145f806624460eed72f093b1d8668b813d8f
Gabriel Scherer <bluestorm.dylc@gmail.com>
2010-10-11 11:50:28 +0200

    Redundant .mli comments (Bat{String,Hashbl,Array}.Cap, BatStream.Labels)  removed.
    .mli comments of modules had been copy-pasted into their Cap/Labels
    variants, effectively creating tons of comment duplication. This
    provoked documentation bugs when modifying some comment without
    updating the Cap/Labels version.
    
    The duplicated comments are now removed, and the user is redirected to
    the base module for documentation.

--- src/batHashtbl.mli
+++ src/batHashtbl.mli
@@ -416,16 +416,11 @@ module Cap :
 sig
 
   type ('a, 'b, 'c) t constraint 'c = [< `Read | `Write ]
-      (** The type of a hashtable. *)
+  (** The type of a hashtable. *)
       
   (**{6 Constructors}*)
 
 val create : int -> ('a, 'b, _) t
-(** [Hashtbl.create n] creates a new, empty hash table, with
-   initial size [n].  For best results, [n] should be on the
-   order of the expected number of elements that will be in
-   the table.  The table grows as needed, so [n] is just an
-   initial guess. *)
 
 external of_table  : ('a, 'b) Hashtbl.t -> ('a, 'b, _ ) t = "%identity"
   (** Adopt a regular hashtable as a capability hashtble, allowing
@@ -455,65 +450,33 @@ external write_only : ('a, 'b, [>`Write]) t -> ('a, 'b, [`Write]) t = "%identity
 
 (**{6 Base operations}*)
 val length : ('a, 'b, _) t -> int
-(** [Hashtbl.length tbl] returns the number of bindings in [tbl].
-   Multiple bindings are counted multiply, so [Hashtbl.length]
-   gives the number of times [Hashtbl.iter] calls its first argument. *)
 
 val is_empty : ('a, 'b, _) t -> bool
-  (** [Hashtbl.is_empty tbl] returns [true] if there are no bindings
-      in [tbl], false otherwise.*)
 
 val add : ('a, 'b, [>`Write]) t -> 'a -> 'b -> unit
-(** [Hashtbl.add tbl x y] adds a binding of [x] to [y] in table [tbl].
-   Previous bindings for [x] are not removed, but simply
-   hidden. That is, after performing {!Hashtbl.remove}[ tbl x],
-   the previous binding for [x], if any, is restored.
-   (Same behavior as with association lists.) *)
 
 val remove : ('a, 'b, [>`Write]) t -> 'a -> unit
-(** [Hashtbl.remove tbl x] removes the current binding of [x] in [tbl],
-   restoring the previous binding if it exists.
-   It does nothing if [x] is not bound in [tbl]. *)
 
 val remove_all : ('a,'b, [>`Write]) t -> 'a -> unit
-  (** Remove all bindings for the given key *)
 
 val replace : ('a, 'b, [>`Write]) t -> 'a -> 'b -> unit
-(** [Hashtbl.replace tbl x y] replaces the current binding of [x]
-   in [tbl] by a binding of [x] to [y].  If [x] is unbound in [tbl],
-   a binding of [x] to [y] is added to [tbl].
-   This is functionally equivalent to {!Hashtbl.remove}[ tbl x]
-   followed by {!Hashtbl.add}[ tbl x y]. *)
 
 val copy : ('a, 'b, [>`Read]) t -> ('a, 'b, _) t
-(** Return a copy of the given hashtable. *)
 
 val clear : ('a, 'b, [>`Write]) t -> unit
-(** Empty a hash table. *)
 
 
 (**{6 Searching}*)
 
 val find : ('a, 'b, [>`Read]) t -> 'a -> 'b
-(** [Hashtbl.find tbl x] returns the current binding of [x] in [tbl],
-   or raises [Not_found] if no such binding exists. *)
 
 val find_all : ('a, 'b, [>`Read]) t -> 'a -> 'b list
-(** [Hashtbl.find_all tbl x] returns the list of all data
-   associated with [x] in [tbl].
-   The current binding is returned first, then the previous
-   bindings, in reverse order of introduction in the table. *)
 
 val find_default : ('a, 'b, [>`Read]) t -> 'a -> 'b -> 'b
-  (** Find a binding for the key, and return a default
-      value if not found *)
 
 val find_option : ('a, 'b, [>`Read]) t -> 'a -> 'b option
-  (** Find a binding for the key, or return [None] if no
-      value is found *)
 
 val mem : ('a, 'b, [>`Read]) t -> 'a -> bool
-(** [Hashtbl.mem tbl x] checks if [x] is bound in [tbl]. *)
 
 (*val exists : ('a,'b) t -> 'a -> bool*)
   (** [exists h k] returns true is at least one item with key [k] is
@@ -521,64 +484,26 @@ val mem : ('a, 'b, [>`Read]) t -> 'a -> bool
 
 (**{6 Traversing}*)
 val iter : ('a -> 'b -> unit) -> ('a, 'b, [>`Read]) t -> unit
-(** [Hashtbl.iter f tbl] applies [f] to all bindings in table [tbl].
-   [f] receives the key as first argument, and the associated value
-   as second argument. Each binding is presented exactly once to [f].
-   The order in which the bindings are passed to [f] is unspecified.
-   However, if the table contains several bindings for the same key,
-   they are passed to [f] in reverse order of introduction, that is,
-   the most recent binding is passed first. *)
 
 val fold : ('a -> 'b -> 'c -> 'c) -> ('a, 'b, [>`Read]) t -> 'c -> 'c
-(** [Hashtbl.fold f tbl init] computes
-   [(f kN dN ... (f k1 d1 init)...)],
-   where [k1 ... kN] are the keys of all bindings in [tbl],
-   and [d1 ... dN] are the associated values.
-   Each binding is presented exactly once to [f].
-   The order in which the bindings are passed to [f] is unspecified.
-   However, if the table contains several bindings for the same key,
-   they are passed to [f] in reverse order of introduction, that is,
-   the most recent binding is passed first. *)
 
 val map : ('a -> 'b -> 'c) -> ('a, 'b, [>`Read]) t -> ('a, 'c, _) t
-  (** [map f x] creates a new hashtable with the same
-      keys as [x], but with the function [f] applied to
-      all the values *)
 
 val filter: ('a -> bool) -> ('key, 'a, [>`Read]) t -> ('key, 'a, _) t
-  (**[filter f m] returns a new hashtable where only the values [a] of [m]
-     such that [f a = true] remain.*)
 
 val filteri: ('key -> 'a -> bool) -> ('key, 'a, [>`Read]) t -> ('key, 'a, _) t
-  (**[filter f m] returns a map where only the key, values pairs
-     [key], [a] of [m] such that [f key a = true] remain. The
-     bindings are passed to [f] in increasing order with respect
-     to the ordering over the type of the keys. *)
 
 val filter_map: ('key -> 'a -> 'b option) -> ('key, 'a, [>`Read]) t -> ('key, 'b, _) t
-  (** [filter_map f m] combines the features of [filteri] and
-      [map].  It calls calls [f key0 a0], [f key1 a1], [f keyn an]
-      where [a0..an] are the elements of [m] and [key0..keyn] the
-      respective corresponding keys. It returns the map of
-      pairs [keyi],[bi] such as [f keyi ai = Some bi] (when [f] returns
-      [None], the corresponding element of [m] is discarded). *)
-
 
 (**{6 Conversions}*)
 
 val keys : ('a,'b, [>`Read]) t -> 'a BatEnum.t
-  (** Return an enumeration of all the keys of a hashtable.
-      If the key is in the Hashtable multiple times, all occurrences
-      will be returned.  *)
 
 val values : ('a, 'b, [>`Read]) t -> 'b BatEnum.t
-  (** Return an enumeration of all the values of a hashtable. *)
 
 val enum : ('a, 'b, [>`Read]) t -> ('a * 'b) BatEnum.t
-  (** Return an enumeration of (key,value) pairs of a hashtable. *)
 
 val of_enum : ('a * 'b) BatEnum.t -> ('a, 'b, _) t
-  (** Create a hashtable from a (key,value) enumeration. *)
 
 (** {6 Boilerplate code}*)
 
@@ -591,27 +516,14 @@ val print :  ?first:string -> ?last:string -> ?sep:string -> ('a BatInnerIO.outp
 
 
 (** {6 Override modules}*)
-  
-(**
-   The following modules replace functions defined in {!Hashtbl} with functions
-   behaving slightly differently but having the same name. This is by design:
-   the functions meant to override the corresponding functions of {!Hashtbl}.
-   *)
-  
-(** Operations on {!Hashtbl} without exceptions.*)
+    
+(** Operations on {!BatHashtbl.Cap} without exceptions.*)
 module Exceptionless :
 sig
   val find : ('a, 'b, [>`Read]) t -> 'a -> 'b option
 end
   
-(** Operations on {!Hashtbl} with labels.
-    
-    This module overrides a number of functions of {!Hashtbl} by
-    functions in which some arguments require labels. These labels are
-    there to improve readability and safety and to let you change the
-    order of arguments to functions. In every case, the behavior of the
-    function is identical to that of the corresponding function of {!Hashtbl}.
-*)
+(** Operations on {!BatHashtbl.Cap} with labels.*)
 module Labels :
 sig
   val add : ('a, 'b, [>`Write]) t -> key:'a -> data:'b -> unit
--- src/batString.mli
+++ src/batString.mli
@@ -563,32 +563,14 @@ type 'a t
 *)
 
 external length : _ t  -> int = "%string_length"
-    (** Return the length (number of characters) of the given string. *)
 
 val is_empty : _ t -> bool 
-(** Determine if a string is empty. *)
 
 external get : [> `Read] t -> int -> char = "%string_safe_get"
-(** [String.get s n] returns character number [n] in string [s].
-   The first character is character number 0.
-   The last character is character number [String.length s - 1].
-   You can also write [s.[n]] instead of [String.get s n].
-
-   Raise [Invalid_argument "index out of bounds"]
-   if [n] is outside the range 0 to [(String.length s - 1)]. *)
-
 
 external set : [> `Write] t -> int -> char -> unit = "%string_safe_set"
-(** [String.set s n c] modifies string [s] in place,
-   replacing the character number [n] by [c].
-   You can also write [s.[n] <- c] instead of [String.set s n c].
-   Raise [Invalid_argument "index out of bounds"]
-   if [n] is outside the range 0 to [(String.length s - 1)]. *)
 
 external create : int -> _ t = "caml_create_string"
-(** [String.create n] returns a fresh string of length [n].
-   The string initially contains arbitrary characters.
-   Raise [Invalid_argument] if [n < 0] or [n > Sys.max_string_length].*)
 
 (** {6 Constructors}*)
 
@@ -605,338 +587,155 @@ external write_only: [> `Write] t -> [`Write] t   = "%identity"
     (** Drop capabilities to write only.*)
 
 val make : int -> char -> _ t
-(** [String.make n c] returns a fresh string of length [n],
-   filled with the character [c].
-   Raise [Invalid_argument] if [n < 0] or [n > ]{!Sys.max_string_length}.*)
 
 val init : int -> (int -> char) -> _ t
-  (** [init l f] returns the string of length [l] with the chars
-      f 0 , f 1 , f 2 ... f (l-1). *)
 
 (** {6 Conversions}*)
 val enum : [> `Read] t -> char BatEnum.t
-  (** Returns an enumeration of the characters of a string.*)
 
 val of_enum : char BatEnum.t -> _ t
-  (** Creates a string from a character enumeration. *)
 
 val backwards : [> `Read] t -> char BatEnum.t
-  (** Returns an enumeration of the characters of a string, from last to first. *)
   
 val of_backwards : char BatEnum.t -> _ t
-  (** Build a string from an enumeration, starting with last character, ending with first. *)
 
 val of_list : char list -> _ t
-  (** Converts a list of characters to a string.*) 
 
 val to_list : [> `Read] t -> char list
-  (** Converts a string to the list of its characters.*)
 
 val of_int : int -> _ t
-  (** Returns the string representation of an int. *)
 
 val of_float : float -> _ t
-  (** Returns the string representation of an float. *)
 
 val of_char : char -> _ t
-  (** Returns a string containing one given character. *)
 
 val to_int : [> `Read] t -> int
-  (** Returns the integer represented by the given string or
-      raises [Failure "int_of_string"] if the string does not represent an integer.*)
 
 val to_float : [> `Read] t -> float
-  (** Returns the float represented by the given string or
-      raises [Failure "float_of_string"] if the string does not represent a float. *)
 
 (** {6 String traversals}*)
 
 val map : (char -> char) -> [>`Read] t -> _ t
-  (** [map f s] returns a string where all characters [c] in [s] have been
-      replaced by [f c]. **)
   
 val fold_left : ('a -> char -> 'a) -> 'a -> [> `Read] t -> 'a
-  (** [fold_left f a s] is
-      [f (... (f (f a s.[0]) s.[1]) ...) s.[n-1]] *)
-val fold_right : (char -> 'a -> 'a) -> [> `Read] t -> 'a -> 'a
-  (** [fold_right f s b] is
-      [f s.[0] (f s.[1] (... (f s.[n-1] b) ...))] *)
 
+val fold_right : (char -> 'a -> 'a) -> [> `Read] t -> 'a -> 'a
 
 val filter : (char -> bool) -> [> `Read] t -> _ t
-  (** [filter f s] returns a copy of string [s] in which only
-      characters [c] such that [f c = true] remain.*)
 
 val filter_map : (char -> char option) -> [> `Read] t -> _ t
-  (** [filter_map f s] calls [(f a0) (f a1).... (f an)] where [a0..an] are
-      the characters of [s]. It returns the string of characters [ci] such as
-      [f ai = Some ci] (when [f] returns [None], the corresponding element of
-      [s] is discarded). *)
 
 
 val iter : (char -> unit) -> [> `Read] t -> unit
-(** [String.iter f s] applies function [f] in turn to all
-   the characters of [s].  It is equivalent to
-   [f s.[0]; f s.[1]; ...; f s.[String.length s - 1]; ()]. *)
 
 
 (** {6 Finding}*)
 
 val index : [>`Read] t -> char -> int
-(** [String.index s c] returns the position of the leftmost
-   occurrence of character [c] in string [s].
-   Raise [Not_found] if [c] does not occur in [s]. *)
 
 val rindex : [> `Read] t -> char -> int
-(** [String.rindex s c] returns the position of the rightmost
-   occurrence of character [c] in string [s].
-   Raise [Not_found] if [c] does not occur in [s]. *)
 
 val index_from : [> `Read] t -> int -> char -> int
-(** Same as {!String.index}, but start
-   searching at the character position given as second argument.
-   [String.index s c] is equivalent to [String.index_from s 0 c].*)
 
 val rindex_from : [> `Read] t -> int -> char -> int
-(** Same as {!String.rindex}, but start
-   searching at the character position given as second argument.
-   [String.rindex s c] is equivalent to
-   [String.rindex_from s (String.length s - 1) c]. *)
 
 val contains : [> `Read] t -> char -> bool
-(** [String.contains s c] tests if character [c]
-   appears in the string [s]. *)
 
 val contains_from : [> `Read] t -> int -> char -> bool
-(** [String.contains_from s start c] tests if character [c]
-   appears in the substring of [s] starting from [start] to the end
-   of [s].
-   Raise [Invalid_argument] if [start] is not a valid index of [s]. *)
 
 val rcontains_from : [> `Read] t -> int -> char -> bool
-(** [String.rcontains_from s stop c] tests if character [c]
-   appears in the substring of [s] starting from the beginning
-   of [s] to index [stop].
-   Raise [Invalid_argument] if [stop] is not a valid index of [s]. *)
-
 
 val find : [> `Read] t -> [> `Read] t -> int
-  (** [find s x] returns the starting index of the string [x]
-      within the string [s] or raises [Not_found] if [x]
-      is not a substring of [s]. *)
 
 val find_from: [> `Read] t -> int -> [> `Read] t -> int
-  (** [find_from s ofs x] behaves as [find s x] but starts searching
-      at offset [ofs]. [find s x] is equivalent to [find_from s 0 x].*)
 
 val rfind : [> `Read] t -> [> `Read] t -> int
-  (** [rfind s x] returns the starting index of the last occurrence
-      of string [x] within string [s].
-
-      {b Note} This implementation is optimized for short strings.
-
-      @raise Not_found if [x] is not a substring of [s]. *)
 
 val rfind_from: [> `Read] t -> int -> [> `Read] t -> int
-  (** [rfind_from s ofs x] behaves as [rfind s x] but starts searching
-      at offset [ofs]. [rfind s x] is equivalent to [rfind_from s (String.length s - 1) x].*)
-
-
 
 val ends_with : [> `Read] t -> [> `Read] t -> bool
-  (** [ends_with s x] returns true if the string [s] is ending with [x]. *)
 
 val starts_with : [> `Read] t -> [> `Read] t -> bool
-  (** [starts_with s x] return true if [s] is starting with [x]. *)
 
 val exists : [> `Read] t -> [> `Read] t -> bool
-  (** [exists str sub] returns true if [sub] is a substring of [str] or
-      false otherwise. *)
 
 (** {6 Transformations}*)
   
 val lchop : [> `Read] t -> _ t
-  (** Returns the same string but without the first character.
-      does nothing if the string is empty. *)
 
 val rchop : [> `Read] t -> _ t
-  (** Returns the same string but without the last character.
-      does nothing if the string is empty. *)
 
 val trim : [> `Read] t -> _ t
-  (** Returns the same string but without the leading and trailing
-      whitespaces. *)
 
 val quote : [> `Read] t -> string
-  (** Add quotes around a string and escape any quote appearing in that string.
-      This function is used typically when you need to generate source code
-      from a string.
-
-      [quote ro"foo"] returns [ro"\"foo\""]
-      [quote ro"\"foo\""] returns [ro"\\\"foo\\\""]
-      etc. *)
 
 val left : [> `Read] t -> int -> _ t
-(**[left r len] returns the string containing the [len] first characters of [r]*)
 
 val right : [> `Read] t -> int -> _ t
-(**[left r len] returns the string containing the [len] last characters of [r]*)
 
 val head : [> `Read] t -> int -> _ t
-(**as {!left}*)
 
 val tail : [> `Read] t -> int -> _ t
-(**[tail r pos] returns the string containing all but the [pos] first characters of [r]*)
-
 
 val strip : ?chars:[> `Read] t -> [> `Read] t -> _ t
-  (** Returns the string without the chars if they are at the beginning or
-      at the end of the string. By default chars are " \t\r\n". *)
 
 val uppercase : [> `Read] t -> _ t
-(** Return a copy of the argument, with all lowercase letters
-   translated to uppercase, including accented letters of the ISO
-   Latin-1 (8859-1) character set. *)
 
 val lowercase : [> `Read] t -> _ t
-(** Return a copy of the argument, with all uppercase letters
-   translated to lowercase, including accented letters of the ISO
-   Latin-1 (8859-1) character set. *)
 
 val capitalize : [> `Read] t -> _ t
-(** Return a copy of the argument, with the first character set to uppercase. *)
 
 val uncapitalize : [> `Read] t -> _ t
-(** Return a copy of the argument, with the first character set to lowercase. *)
 
 val copy : [> `Read] t -> _ t
-(** Return a copy of the given string. *)
 
 val sub : [> `Read] t -> int -> int -> _ t
-(** [String.sub s start len] returns a fresh string of length [len],
-   containing the characters number [start] to [start + len - 1]
-   of string [s].
-   Raise [Invalid_argument] if [start] and [len] do not
-   designate a valid substring of [s]; that is, if [start < 0],
-   or [len < 0], or [start + len > ]{!String.length}[ s]. *)
 
 val fill : [> `Write] t -> int -> int -> char -> unit
-(** [String.fill s start len c] modifies string [s] in place,
-   replacing the characters number [start] to [start + len - 1]
-   by [c].
-   Raise [Invalid_argument] if [start] and [len] do not
-   designate a valid substring of [s]. *)
 
 val blit : [> `Read] t -> int -> [> `Write] t -> int -> int -> unit
-(** [String.blit src srcoff dst dstoff len] copies [len] characters
-   from string [src], starting at character number [srcoff], to
-   string [dst], starting at character number [dstoff]. It works
-   correctly even if [src] and [dst] are the same string,
-   and the source and destination chunks overlap.
-   Raise [Invalid_argument] if [srcoff] and [len] do not
-   designate a valid substring of [src], or if [dstoff] and [len]
-   do not designate a valid substring of [dst]. *)
 
 val concat : [> `Read] t -> [> `Read] t list -> _ t
-(** [String.concat sep sl] concatenates the list of strings [sl],
-   inserting the separator string [sep] between each. *)
 
 val escaped : [> `Read] t -> _ t
-(** Return a copy of the argument, with special characters
-   represented by escape sequences, following the lexical
-   conventions of Objective Caml.  If there is no special
-   character in the argument, return the original string itself,
-   not a copy. *)
 
 val replace_chars : (char -> [> `Read] t) -> [> `Read] t -> _ t
-  (** [replace_chars f s] returns a string where all chars [c] of [s] have been
-      replaced by the string returned by [f c]. *)
 
 val replace : str:[> `Read] t -> sub:[> `Read] t -> by:[> `Read] t -> bool * _ t
-  (** [replace ~str ~sub ~by] returns a tuple constisting of a boolean
-      and a string where the first occurrence of the string [sub]
-      within [str] has been replaced by the string [by]. The boolean
-      is true if a subtitution has taken place. *)
 
 val repeat: [> `Read] t -> int -> _ t
-(** [repeat s n] returns [s ^ s ^ ... ^ s] *)
 
 (** {6 Splitting around}*)
 val split : [> `Read] t -> [> `Read] t -> _ t * _ t
-  (** [split s sep] splits the string [s] between the first
-      occurrence of [sep].
-      raises [Not_found] if the separator is not found. *)
 
 val rsplit : [> `Read] t -> string -> string * string
-  (** [rsplit s sep] splits the string [s] between the last
-      occurrence of [sep].
-      raises [Not_found] if the separator is not found. *)
 
 val nsplit : [> `Read] t -> [> `Read] t -> _ t list
-  (** [nsplit s sep] splits the string [s] into a list of strings
-      which are separated by [sep].
-      [nsplit "" _] returns the empty list. *)
 
 val splice: [ `Read | `Write] t  -> int -> int -> [> `Read] t -> string
-  (** [String.splice s off len rep] cuts out the section of [s]
-      indicated by [off] and [len] and replaces it by [rep] *)
 
 val join : [> `Read] t -> [> `Read] t list -> _ t
-  (** Same as [concat] *)
 
 val slice : ?first:int -> ?last:int -> [> `Read] t -> _ t
-  (** [slice ?first ?last s] returns a "slice" of the string
-      which corresponds to the characters [s.[first]],
-      [s.[first+1]], ..., [s[last-1]]. Note that the character at
-      index [last] is {b not} included! If [first] is omitted it
-      defaults to the start of the string, i.e. index 0, and if
-      [last] is omitted is defaults to point just past the end of
-      [s], i.e. [length s].  Thus, [slice s] is equivalent to
-      [copy s].
-      
-      Negative indexes are interpreted as counting from the end of
-      the string. For example, [slice ~last:-2 s] will return the
-      string [s], but without the last two characters.
-      
-      This function {b never} raises any exceptions. If the
-      indexes are out of bounds they are automatically clipped.
-  *)
-
-
 
 val explode : [> `Read] t -> char list
-  (** [explode s] returns the list of characters in the string [s]. *)
+
 val implode : char list -> _ t
-  (** [implode cs] returns a string resulting from concatenating
-      the characters in the list [cs]. *)
 
 (** {6 Comparisons}*)
 
 val compare: [> `Read] t -> [> `Read] t -> int
-  (** The comparison function for strings, with the same specification as
-      {!Pervasives.compare}.  Along with the type [t], this function [compare]
-      allows the module [String] to be passed as argument to the functors
-      {!Set.Make} and {!Map.Make}. *)
 
 val icompare: [> `Read] t -> [> `Read] t -> int
-  (** Compare two strings, case-insensitive. *)
 
 
 (** {7 Printing}*)
 
 val print: 'a BatInnerIO.output -> [> `Read] t -> unit
-(**Print a string.*)
 
 val println: 'a BatInnerIO.output -> [> `Read] t -> unit
-(**Print a string, end the line.*)
 
 val print_quoted: 'a BatInnerIO.output -> [> `Read] t -> unit
-(**Print a string, with quotes.
-
-   [print_quoted stdout "foo"] prints ["foo"] (with the quotes)
-
-   [print_quoted stdout "\"bar\""] prints ["\"bar\""] (with the quotes)
-*)
 
 val t_printer : [> `Read] t BatValue_printer.t
 
@@ -956,68 +755,30 @@ external unsafe_fill :
 module Exceptionless :
 sig
   val to_int : [> `Read] t -> int option
-  (** Returns the integer represented by the given string or
-      [None] if the string does not represent an integer.*)
 
   val to_float : [> `Read] t -> float option
-  (** Returns the float represented by the given string or
-      [None] if the string does not represent a float. *)
 
   val index : [>`Read] t -> char -> int option
-  (** [index s c] returns [Some p], the position of the leftmost
-      occurrence of character [c] in string [s] or
-      [None] if [c] does not occur in [s]. *)
 
   val rindex : [> `Read] t -> char -> int option
-  (** [rindex s c] returns [Some p], the position of the rightmost
-      occurrence of character [c] in string [s] or
-      [None] if [c] does not occur in [s]. *)
 
   val index_from : [> `Read] t -> int -> char -> int option
-  (** Same as {!String.Exceptionless.index}, but start
-      searching at the character position given as second argument.
-      [index s c] is equivalent to [index_from s 0 c].*)
 
   val rindex_from : [> `Read] t -> int -> char -> int option
-  (** Same as {!String.Exceptionless.rindex}, but start
-      searching at the character position given as second argument.
-      [rindex s c] is equivalent to
-      [rindex_from s (String.length s - 1) c]. *)
 
   val find : [> `Read] t -> [> `Read] t -> int option
-  (** [find s x] returns [Some i], the starting index of the first
-      occurrence of string [x] within string [s], or [None] if [x]
-      is not a substring of [s].
-
-      {b Note} This implementation is optimized for short strings. *)
 
   val find_from: [> `Read] t -> int -> [> `Read] t -> int option
-  (** [find_from s ofs x] behaves as [find s x] but starts searching
-      at offset [ofs]. [find s x] is equivalent to [find_from s 0 x].*)
 
   val rfind : [> `Read] t -> [> `Read] t -> int option
-  (** [rfind s x] returns [Some i], the starting index of the last occurrence
-      of string [x] within string [s], or [None] if [x] is not a substring
-      of [s].
-
-      {b Note} This implementation is optimized for short strings. *)
 
   val rfind_from: [> `Read] t -> int -> [> `Read] t -> int option
-  (** [rfind_from s ofs x] behaves as [rfind s x] but starts searching
-      at offset [ofs]. [rfind s x] is equivalent to
-      [rfind_from s (String.length s - 1) x]. *)
 
   (* val split : string -> string -> (string * string) option TODO *)
   val split : [> `Read] t -> [> `Read] t -> (_ t * _ t) option
-  (** [split s sep] splits the string [s] between the first
-      occurrence of [sep], or returns [None] if the separator
-      is not found. *)
 
 (*   val rsplit : string -> string -> (string * string) option TODO *)
   val rsplit : [> `Read] t -> [> `Read] t -> (_ t * _ t) option
-  (** [rsplit s sep] splits the string [s] between the last
-      occurrence of [sep], or returns [None] if the separator
-      is not found. *)
 
 end (* String.Cap.Exceptionless *)
 
