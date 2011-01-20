24aea8495ed66abe6cd2bf423a5c14a7e6088ab0
Eric Norige <thelema314@gmail.com>
2011-01-20 13:47:50 -0500

    try a hack on pa_estring to fix the type error in the toplevel

--- libs/estring/pa_estring_top.ml
+++ libs/estring/pa_estring_top.ml
@@ -11,5 +11,20 @@
 
 open Camlp4.PreCast
 
+
+(* Hack used to force camlp4 to define a new token stream filtered
+   with the newly defined filter. *)
+let _ =
+  let parse = !Toploop.parse_toplevel_phrase and lexbufs = ref [] in
+  Toploop.parse_toplevel_phrase :=
+    (fun lexbuf ->
+      match try Some(List.assq lexbuf !lexbufs) with _ -> None with
+	| Some lexbuf ->
+	  parse lexbuf
+	| None ->
+	  let new_lexbuf = { lexbuf with Lexing.refill_buff = lexbuf.Lexing.refill_buff } in
+	  lexbufs := (lexbuf, new_lexbuf) :: !lexbufs;
+	  parse new_lexbuf)
+
 (* Reload "Camlp4Top.cmo" to force camlp4 to use the new topphrase filter *)
-let _ = Topdirs.dir_load Format.std_formatter "Camlp4Top.cmo"
+(*let _ = Topdirs.dir_load Format.std_formatter "Camlp4Top.cmo" *)
