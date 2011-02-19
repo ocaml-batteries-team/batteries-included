7b80d8b72592967e0e25ef4f6dda7c71c3bb4820
Ashish Agarwal <agarwal1975@gmail.com>
2011-02-19 15:07:21 -0500

    In Tuple2, implemented new functions and made changes to previous API.

--- src/batTuple2.ml
+++ src/batTuple2.ml
@@ -24,30 +24,42 @@ type ('a,'b) t = 'a * 'b
 type 'a enumerable = 'a * 'a
 type 'a mappable = 'a * 'a
 
+external fst : 'a * 'b -> 'a = "%field0"
+external snd : 'a * 'b -> 'b = "%field1"
+
 let map f (x,y) =
   (* force left-to-right evaluation order (this principle of least
      surprise is already applied in stdlib's List.map) *)
   let a = f x in
   (a, f y)
 
-let compare ?(c1=Pervasives.compare) ?(c2=Pervasives.compare) (a,b) (c,d) = 
-  let comp = c1 a c in 
-  if comp <> 0 then comp else c2 b d
+let mapn f g (a,b) =
+  let a = f a in
+  (a, g b)
+
+let map1 f (a,b) = (f a, b)
+let map2 f (a,b) = (a, f b)
+
+let curry f x y = f (x,y)
+let uncurry f (x,y) = f x y
 
 let enum (x,y) = BatList.enum [x;y] (* Make efficient? *)
 
 let of_enum e = match BatEnum.get e with 
-    None -> failwith "Pair.of_enum: not enough elements" 
+    None -> failwith "Tuple2.of_enum: not enough elements" 
   | Some x -> match BatEnum.get e with 
-	None -> failwith "Pair.of_enum: not enough elements" 
+	None -> failwith "Tuple2.of_enum: not enough elements" 
       | Some y -> (x,y)
 
-let print print_a print_b out (a,b) = 
+let printn print_a print_b out (a,b) = 
   BatIO.write out '(';
   print_a out a;
   BatIO.write out ',';
   print_b out b;
   BatIO.write out ')'
 
-let print2 printer out pair = print printer printer out pair
+let print printer out pair = printn printer printer out pair
   
+let compare ?(cmp1=Pervasives.compare) ?(cmp2=Pervasives.compare) (a,b) (c,d) = 
+  let comp = cmp1 a c in 
+  if comp <> 0 then comp else cmp2 b d
--- src/batTuple2.mli
+++ src/batTuple2.mli
@@ -18,31 +18,64 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-(** Functions for pairs of values
+(** Pairs of values. Several of the functions here are also exposed in
+    {!Pervasives}, as documented below.
 
     @author Edgar Friendly
 *)
 
 type ('a,'b) t = 'a * 'b
 
-(** map a function across both values in a pair *)
+external fst : 'a * 'b -> 'a = "%field0"
+    (** Project out first element of a pair. Equivalent to
+        {!Pervasives.fst}. *)
+
+external snd : 'a * 'b -> 'b = "%field1"
+    (** Project out second element of a pair. Equivalent to
+        {!Pervasives.snd}. *)
+
 val map : ('a -> 'b) -> ('a * 'a) -> ('b * 'b)
+  (** Map all values in a pair with the same function.
+      [map f (a,b) = (f a, f b)]. *)
 
-(** Compare two pairs in lexicographic order, possibly using custom comparators for the two fields *)
-val compare : ?c1:('a -> 'a -> int) -> ?c2:('b -> 'b -> int) -> ('a * 'b) -> ('a * 'b) -> int
+val mapn : ('a -> 'c) -> ('b -> 'd) -> 'a * 'b -> 'c * 'd
+  (** Map all values in a pair with different functions.
+      [mapn f g (a,b) = (f a, g b)]. Equivalent to
+      {!BatPervasives.(***)}. *)
 
-(** builds a two-element enum of a pair *)
+val map1 : ('a -> 'c) -> ('a * 'b) -> ('c * 'b)
+  (** Map first item in a pair. [map1 f (a,b) = (f a, b)]. *)
+
+val map2 : ('b -> 'c) -> ('a * 'b) -> ('a * 'c)
+  (** Map second item in a pair. [map2 f (a,b) = (a, f b)]. *)
+
+val curry : ('a * 'b -> 'c) -> 'a -> 'b -> 'c
+  (** Convert a function that accepts a pair of arguments into a
+      function that accepts two arguments. Equivalent to
+      {!Pervasives.curry}. *)
+
+val uncurry : ('a -> 'b -> 'c) -> 'a * 'b -> 'c
+  (** Convert a function that accepts two arguments into a function
+      that accepts a pair of arguments. Equivalent to
+      {!Pervasives.uncurry}. *)
+
 val enum : ('a * 'a) -> 'a BatEnum.t
+  (** Build a two-element enum from a pair. *)
 
-(** builds a pair out of the first two values of an enum.  Raises [Failure] if insufficient elements *)
 val of_enum : 'a BatEnum.t -> ('a * 'a)
+  (** Build a pair out of the first two elements of an enum. Raises
+      [Failure] if insufficient elements. *)
 
-(** Prints a pair using given printing functions *)
-val print : ('o BatIO.output -> 'a -> unit) -> ('o BatIO.output -> 'b -> unit) -> 'o BatIO.output -> ('a * 'b) -> unit
+val printn : ('o BatIO.output -> 'a -> unit) -> ('o BatIO.output -> 'b -> unit) -> 'o BatIO.output -> ('a * 'b) -> unit
+  (** Print a pair using given printing functions. *)
 
-val print2 : ('o BatIO.output -> 'a -> unit) -> 'o BatIO.output -> ('a * 'a) -> unit
+val print : ('o BatIO.output -> 'a -> unit) -> 'o BatIO.output -> ('a * 'a) -> unit
+  (** Print a pair using given printing function. Like [printn] but
+      elements must be of same type. *)
 
+val compare : ?cmp1:('a -> 'a -> int) -> ?cmp2:('b -> 'b -> int) -> ('a * 'b) -> ('a * 'b) -> int
+  (** Compare two pairs in lexicographic order, possibly using custom
+      comparators for the two fields. *)
 
 include BatEnum.Enumerable with type 'a enumerable = 'a * 'a
 include BatInterfaces.Mappable with type 'a mappable = 'a * 'a
-
