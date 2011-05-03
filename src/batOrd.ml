f87af5c121419c4957e0799c433f74cc26a10d71
Gabriel Scherer <bluestorm.dylc@gmail.com>
2011-05-03 02:53:15 +0200

    an experimentation with "module classes" for equality and comparison
    We provide two comparison types, the legacy `type 'a comp = 'a -> 'a -> int`,
    as well as the nicer to use `type 'a ord = 'a -> 'a -> (Lt | Eq | Gt)`.
    Conversion combinators back and forth should make them both equally
    easy to use.

---
+++ src/batOrd.ml
@@ -0,0 +1,140 @@
+module Incubator = struct
+
+type order = Lt | Eq | Gt
+
+type 'a comp = 'a -> 'a -> int
+type 'a ord = 'a -> 'a -> order
+
+module type Comp = sig
+  type t
+  val compare : t comp
+end
+
+module type Ord = sig
+  type t
+  val ord : t ord
+end
+
+let ord0 n =
+  if n < 0 then Lt
+  else if n > 0 then Gt
+  else Eq
+
+let ord comp = fun a b -> ord0 (comp a b)
+
+let poly_comp = Pervasives.compare
+
+(* eta-expand to avoid value restriction *)
+let poly_ord = fun a b -> ord poly_comp a b
+
+let poly = poly_ord
+
+module Ord (Comp : Comp) : Ord with type t = Comp.t = struct
+  type t = Comp.t
+  let ord = ord Comp.compare
+end
+
+let comp0 = function
+  | Lt -> -1
+  | Eq -> 0
+  | Gt -> 1
+
+let comp ord = fun a b -> comp0 (ord a b)
+
+module Comp (Ord : Ord) : Comp with type t = Ord.t = struct
+  type t = Ord.t
+  let compare = comp Ord.ord
+end
+
+let rev_ord0 = function
+  | Lt -> Gt
+  | Eq -> Eq
+  | Gt -> Lt
+
+let rev_comp0 n =
+  if n < 0 then 1
+  else if n > 0 then -1
+  else 0
+
+let rev_ord ord = fun a b -> rev_ord0 (ord a b)
+let rev_comp comp = fun a b -> rev_comp0 (comp a b)
+
+let rev = rev_ord
+
+module RevOrd (Ord : Ord) : Ord with type t = Ord.t = struct
+  type t = Ord.t
+  let ord = rev Ord.ord
+end
+
+module RevComp (Comp : Comp) : Comp with type t = Comp.t = struct
+  type t = Comp.t
+  let compare = rev_comp Comp.compare
+end
+
+module Rev = RevOrd
+
+type 'a eq = 'a -> 'a -> bool
+
+let eq_ord0 = function
+  | Eq -> true
+  | Lt | Gt -> false
+
+let eq_comp0 = function
+  | 0 -> true
+  | _ -> false
+
+let eq_ord ord = fun a b -> eq_ord0 (ord a b)
+let eq_comp comp = fun a b -> eq_comp0 (comp a b)
+
+let eq = eq_ord
+
+module type Eq = sig
+  type t
+  val eq : t eq
+end
+
+module EqOrd (Ord : Ord) : Eq with type t = Ord.t = struct
+  type t = Ord.t
+  let eq = eq_ord Ord.ord
+end
+
+module EqComp (Comp : Comp) : Eq with type t = Comp.t = struct
+  type t = Comp.t
+  let eq = eq_comp Comp.compare
+end
+
+module Eq = EqOrd
+
+type 'a choice = 'a -> 'a -> 'a
+
+let min_ord ord = fun a b ->
+  match ord a b with
+    | Lt | Eq -> a
+    | Gt -> b
+
+let min_comp comp = fun a b ->
+  if comp a b <= 0 then a else b
+
+(**T max
+   Incubator.max_ord Incubator.poly_ord 1 2 = 2
+   Incubator.max_comp Incubator.poly_comp 1 2 = 2
+**)
+let max_ord ord = min_ord (rev_ord ord)
+let max_comp comp = min_comp (rev_comp comp)
+
+let min = min_ord
+let max = max_ord
+
+let bin_eq eq1 t1 t1' eq2 t2 t2' =
+  eq1 t1 t1' && eq2 t2 t2'
+
+let bin_ord ord1 t1 t1' ord2 t2 t2' =
+  match ord1 t1 t1' with
+    | Eq -> ord2 t2 t2'
+    | (Lt | Gt) as neq -> neq
+
+let bin_comp comp1 t1 t1' comp2 t2 t2' =
+  match comp1 t1 t1' with
+    | 0 -> comp2 t2 t2'
+    | nzero -> nzero
+end
