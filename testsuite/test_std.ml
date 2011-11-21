8652e24a1fb3925e383bc397e3628e2f642502bf
Gabriel Scherer <bluestorm.dylc@gmail.com>
2011-11-21 16:42:32 +0100

    tests added for Std.dump

--- testsuite/test_std.ml
+++ testsuite/test_std.ml
@@ -12,6 +12,103 @@ let test_using () =
   assert_equal ~printer 7 (!run);
   assert_equal ~printer 5 (!closed)
 
+
+type test1 = 
+  | A of int
+  | B of float * float
+  | C of string * test1
+
+type test2 = {
+  a : int;
+  b : float * float;
+  c : string * test2 option;
+}
+
+type test3 = {
+  f1 : float;
+  f2 : float;
+  f3 : float;
+}
+
+let test_dump () =
+  let test str value =
+    assert_equal ~msg:str ~printer:(fun x -> x) str (BatStd.dump value) in
+
+  (* integers *)
+  test "0" None;
+  test "0" false;
+  test "1" true;
+  test "17" 17;
+  
+  (* lists *)
+  (* despite the specialized list-spotting routine, [] is printed as
+     0 as they have the same representation *)
+  test "0" [];
+  test "[1; 2]" [1; 2];
+
+  (* algebraic datatypes *)
+  test "(1)" (A 1);
+  test "Tag1 (2., 3.)" (B (2.,3.));
+  test "Tag2 (\"foo\", (1))" (C ("foo", A 1));
+
+  test "(1, (2., 3.), [\"foo\"])" {a = 1; b = (2., 3.); c = "foo", None};
+
+  (* lazy *)
+  (* lazy immediate values are not lazyfied!
+     test "0" (lazy 0); *)
+  test "<lazy>" (lazy (ignore ()));
+
+  (* closures *)
+  test "<closure>" (fun x -> x);
+  
+  (* objects *)
+  let obj = object
+    val x = 2
+    val z = 3.
+    method foo = "bar" end in
+  test (Printf.sprintf "Object #%d (2, 3.)" (Oo.id obj)) obj;
+
+  (* infix, forward? *)
+  
+  (* string *)
+  let str = "foo \"bar\"\n" in
+  test (Printf.sprintf "%S" str) str;
+
+  (* double *)
+  let test_float x = test (string_of_float x) x in
+  List.iter test_float
+    [0.; 1.; -2.; max_float; min_float; epsilon_float; nan];
+  for i = 0 to 1000 do
+    test_float (Random.float max_float);
+    test_float (Random.float min_float);
+  done;
+
+  (* abstract? *)
+
+  (* custom? *)
+
+  (* final? *)
+
+
+  (* double array or struct *)
+  test "()" ([| |] : float array);
+  test "[| 0.; 1.; 2. |]" [| 0.; 1.; 2. |];
+  test "[| 0.; 1.; 2. |]" { f1 = 0.; f2 = 1.; f3 = 2. };
+
+  (* resilience *)
+  (* commented: this actually raises an assert failure in the Std.dump code
+  let () =
+    let x = A 1 in
+    for i = -257 to 257 do
+      Obj.set_tag (Obj.repr x) i;
+      ignore (Std.dump x);
+    done
+  in
+  *)
+
+  ()
+
 let tests = "Std" >::: [
-  "using" >:: test_using
+  "using" >:: test_using;
+  "dump" >:: test_dump;
 ];;
