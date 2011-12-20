7621e7c6cd55ae8b6eb0d35eba7404976989cf9f
Eric Norige <thelema314@gmail.com>
2011-12-20 12:06:17 -0500

    Add more numeric constants, reformat batFloat.mli

--- src/batFloat.mli
+++ src/batFloat.mli
@@ -44,198 +44,246 @@
    @documents Float
 *)
 
-    type t = float
-	(**The type of floating-point numbers.
+type t = float
+(**The type of floating-point numbers.
 
-	   Floating-point numbers are the default representation of
-	   real numbers by OCaml. *)
+   Floating-point numbers are the default representation of
+   real numbers by OCaml. *)
 
-    (**
-       {6 Usual operations}
-    *)
+(**
+   {6 Usual operations}
+ *)
 
-    val zero : float
-      (** Floating number zero. This is the same thing as [0.]*)
+val zero : float
+(** Floating number zero. This is the same thing as [0.]*)
 
-    val one : float
-      (** Floating number one. This is the same thing as [1.]*)
+val one : float
+(** Floating number one. This is the same thing as [1.]*)
 
-    val neg : float -> float
+val neg : float -> float
+(** Returns the negation of the input, i.e. (fun x -> ~-. x) *)
 
-    val succ : float -> float
-      (** Add [1.] to a floating number. Note that, as per IEEE 754,
-	  if [x] is a large enough float number, [succ x] might be
-	  equal to [x], due to rounding.*)
+val succ : float -> float
+(** Add [1.] to a floating number. Note that, as per IEEE 754,
+    if [x] is a large enough float number, [succ x] might be
+    equal to [x], due to rounding.*)
 
-    val pred : float -> float
-      (** Substract [1.] from a floating number. Note that, as per
-	  IEEE 754, if [x] is a large enough float number, [pred x]
-	  might be equal to [x], due to rounding.*)
+val pred : float -> float
+(** Substract [1.] from a floating number. Note that, as per
+    IEEE 754, if [x] is a large enough float number, [pred x]
+    might be equal to [x], due to rounding.*)
 
-    val abs : float -> float
-      (** The absolute value of a floating point number.*)
+val abs : float -> float
+(** The absolute value of a floating point number.*)
 
-    val add : float -> float -> float
-    val sub : float -> float -> float
-    val mul : float -> float -> float
-    val div : float -> float -> float
-    val modulo : float -> float -> float
-    val pow : float -> float -> float
-    val min_num : float
-    val max_num : float
-    val compare : float -> float -> int
-    val of_int : int -> float
-    val to_int : float -> int
-    external of_float : float -> float = "%identity"
-    external to_float : float -> float = "%identity"
-    val of_string : string -> float
-    val to_string : float -> string
-    val ( + ) : t -> t -> t
-    val ( - ) : t -> t -> t
-    val ( * ) : t -> t -> t
-    val ( / ) : t -> t -> t
-    val ( ** ) : t -> t -> t
-    val ( <> ) : t -> t -> bool
-    val ( >= ) : t -> t -> bool
-    val ( <= ) : t -> t -> bool
-    val ( > ) : t -> t -> bool
-    val ( < ) : t -> t -> bool
-    val ( = ) : t -> t -> bool
-    val ( -- ): t -> t -> t BatEnum.t
-    val ( --- ): t -> t -> t BatEnum.t
-    val operations : t BatNumber.numeric
+val add : float -> float -> float
+val sub : float -> float -> float
+val mul : float -> float -> float
+val div : float -> float -> float
+val modulo : float -> float -> float
+val pow : float -> float -> float
+val min_num : float
+val max_num : float
+val compare : float -> float -> int
+val of_int : int -> float
+val to_int : float -> int
+external of_float : float -> float = "%identity"
+external to_float : float -> float = "%identity"
+val of_string : string -> float
+val to_string : float -> string
+val ( + ) : t -> t -> t
+val ( - ) : t -> t -> t
+val ( * ) : t -> t -> t
+val ( / ) : t -> t -> t
+val ( ** ) : t -> t -> t
+val ( <> ) : t -> t -> bool
+val ( >= ) : t -> t -> bool
+val ( <= ) : t -> t -> bool
+val ( > ) : t -> t -> bool
+val ( < ) : t -> t -> bool
+val ( = ) : t -> t -> bool
+val ( -- ): t -> t -> t BatEnum.t
+val ( --- ): t -> t -> t BatEnum.t
+val operations : t BatNumber.numeric
 
-    (**
-       {6 Operations specific to floating-point numbers}
-    *)
+(**
+   {6 Operations specific to floating-point numbers}
+ *)
 
-    external exp : float -> float = "caml_exp_float" "exp" "float"
-      (** Exponential. *)
+external exp : float -> float = "caml_exp_float" "exp" "float"
+(** Exponential. *)
 
-    external log : float -> float = "caml_log_float" "log" "float"
-      (** Natural logarithm. *)
+external log : float -> float = "caml_log_float" "log" "float"
+(** Natural logarithm. *)
 
-    external log10 : float -> float = "caml_log10_float" "log10" "float"
-      (** Base 10 logarithm. *)
+external log10 : float -> float = "caml_log10_float" "log10" "float"
+(** Base 10 logarithm. *)
 
-    external cos : float -> float = "caml_cos_float" "cos" "float"
-      (** See {!atan2}. *)
+external cos : float -> float = "caml_cos_float" "cos" "float"
+(** See {!atan2}. *)
 
-    external sin : float -> float = "caml_sin_float" "sin" "float"
-      (** See {!atan2}. *)
+external sin : float -> float = "caml_sin_float" "sin" "float"
+(** See {!atan2}. *)
 
-    external tan : float -> float = "caml_tan_float" "tan" "float"
-      (** See {!atan2}. *)
+external tan : float -> float = "caml_tan_float" "tan" "float"
+(** See {!atan2}. *)
 
-    external acos : float -> float = "caml_acos_float" "acos" "float"
-      (** See {!atan2}. *)
+external acos : float -> float = "caml_acos_float" "acos" "float"
+(** See {!atan2}. *)
 
-    external asin : float -> float = "caml_asin_float" "asin" "float"
-      (** See {!atan2}. *)
+external asin : float -> float = "caml_asin_float" "asin" "float"
+(** See {!atan2}. *)
 
-    external atan : float -> float = "caml_atan_float" "atan" "float"
-      (** See {!atan2}. *)
+external atan : float -> float = "caml_atan_float" "atan" "float"
+(** See {!atan2}. *)
 
-    external atan2 : float -> float -> float = "caml_atan2_float" "atan2" "float"
-      (** The usual trigonometric functions. *)
+external atan2 : float -> float -> float = "caml_atan2_float" "atan2" "float"
+(** The usual trigonometric functions. *)
 
-    external cosh : float -> float = "caml_cosh_float" "cosh" "float"
-      (** See {!tanh}. *)
+external cosh : float -> float = "caml_cosh_float" "cosh" "float"
+(** See {!tanh}. *)
 
-    external sinh : float -> float = "caml_sinh_float" "sinh" "float"
-      (** See {!tanh}. *)
+external sinh : float -> float = "caml_sinh_float" "sinh" "float"
+(** See {!tanh}. *)
 
-    external tanh : float -> float = "caml_tanh_float" "tanh" "float"
-      (** The usual hyperbolic trigonometric functions. *)
+external tanh : float -> float = "caml_tanh_float" "tanh" "float"
+(** The usual hyperbolic trigonometric functions. *)
 
-    external ceil : float -> float = "caml_ceil_float" "ceil" "float"
-      (** See {!floor}. *)
+external ceil : float -> float = "caml_ceil_float" "ceil" "float"
+(** See {!floor}. *)
 
-    external floor : float -> float = "caml_floor_float" "floor" "float"
-      (** Round the given float to an integer value.
-	  [floor f] returns the greatest integer value less than or
-	  equal to [f].
-	  [ceil f] returns the least integer value greater than or
-	  equal to [f]. *)
+external floor : float -> float = "caml_floor_float" "floor" "float"
+(** Round the given float to an integer value.
+    [floor f] returns the greatest integer value less than or
+    equal to [f].
+    [ceil f] returns the least integer value greater than or
+    equal to [f]. *)
 
-    val round : float -> float
-    (** [round x] rounds [x] to the nearest integral floating-point
-        (the nearest of [floor x] and [ceil x]). In case the fraction
-        of x is exactly 0.5, we round away from 0. : [round 1.5] is
-        [2.] but [round (-3.5)] is [-4.]. *)
+val round : float -> float
+(** [round x] rounds [x] to the nearest integral floating-point
+    (the nearest of [floor x] and [ceil x]). In case the fraction
+    of x is exactly 0.5, we round away from 0. : [round 1.5] is
+    [2.] but [round (-3.5)] is [-4.]. *)
 
-    val round_to_int : float -> int
-    (** [round_to_int x] is [int_of_float (round x)].
+val round_to_int : float -> int
+(** [round_to_int x] is [int_of_float (round x)].
 
-        @since 2.0 *)
+    @since 2.0 *)
 
-    val round_to_string : ?digits:int -> float -> string
-    (** [round_to_string ~digits:d x] will return a string
-        representation of [x] -- in base 10 -- rounded to [d] digits
-        after the decimal point. By default, [digits] is [0], we round
-        to the nearest integer.
-        
-        @raise Invalid_argument if the ~digits argument is negative.
+val round_to_string : ?digits:int -> float -> string
+(** [round_to_string ~digits:d x] will return a string
+    representation of [x] -- in base 10 -- rounded to [d] digits
+    after the decimal point. By default, [digits] is [0], we round
+    to the nearest integer.
 
-        This is strictly a convenience function for simple end-user
-        printing and you should not rely on its behavior. One possible
-        implementation is to rely on C `sprintf` internally, which
-        means:
+    @raise Invalid_argument if the ~digits argument is negative.
 
-        - no guarantee is given on the round-at-half behavior; it may
-          not be consistent with [round] or [round_to_int]
-        
-        - [round_to_string ~digits:0 3.] may return "3" instead of
-          "3." as [string_of_float] would
+    This is strictly a convenience function for simple end-user
+    printing and you should not rely on its behavior. One possible
+    implementation is to rely on C `sprintf` internally, which
+    means:
 
-        - no guarantee is given on the behavior for abusively high
-          number of digits precision; for example [round_to_string
-          ~digits:max_int x] may return the empty string.
+    - no guarantee is given on the round-at-half behavior; it may
+    not be consistent with [round] or [round_to_int]
 
-        @since 2.0 *)
+    - [round_to_string ~digits:0 3.] may return "3" instead of
+    "3." as [string_of_float] would
 
-    val infinity : float
-      (** Positive infinity. *)
-      
-    val neg_infinity : float
-      (** Negative infinity. *)
+    - no guarantee is given on the behavior for abusively high
+    number of digits precision; for example [round_to_string
+    ~digits:max_int x] may return the empty string.
 
-    val nan : float
-      (** A special floating-point value denoting the result of an
-	  undefined operation such as [0.0 /. 0.0].  Stands for
-	  ``not a number''.  Any floating-point operation with [nan] as
-	  argument returns [nan] as result.  As for floating-point comparisons,
-	  [=], [<], [<=], [>] and [>=] return [false] and [<>] returns [true]
-	  if one or both of their arguments is [nan]. *)
+    @since 2.0 *)
 
-    val is_nan : float -> bool
-      (** [is_nan f] returns [true] if [f] is [nan], [false] otherwise.*)
+val is_nan : float -> bool
+(** [is_nan f] returns [true] if [f] is [nan], [false] otherwise.*)
 
-    val is_special : float -> bool
-      (** [is_special f] returns [true] if [f] is [nan] or [+/- infinity],
-          [false] otherwise.
+val is_special : float -> bool
+(** [is_special f] returns [true] if [f] is [nan] or [+/- infinity],
+    [false] otherwise.
 
-          @since 2.0 *)
+    @since 2.0 *)
 
-    val epsilon : float
-      (** The smallest positive float [x] such that [1.0 +. x <> 1.0]. *)
+(** {6 Constants} *)
 
-    val pi : float
-      (** The constant pi (3.14159...) *)
+(** Special float constants.  It may not be safe to compare
+    directly with these, as they have multiple internal
+    representations.  Instead use the [is_special], [is_nan],
+    etc. tests *)
 
-    (** {6 Operations on the internal representation of floating-point numbers}*)
+val infinity : float
+(** Positive infinity. *)
 
-    external frexp : float -> float * int = "caml_frexp_float"
-      (** [frexp f] returns the pair of the significant
-	  and the exponent of [f].  When [f] is zero, the
-	  significant [x] and the exponent [n] of [f] are equal to
-	  zero.  When [f] is non-zero, they are defined by
-	  [f = x *. 2 ** n] and [0.5 <= x < 1.0]. *)
+val neg_infinity : float
+(** Negative infinity. *)
 
-    external ldexp : float -> int -> float = "caml_ldexp_float"
-      (** [ldexp x n] returns [x *. 2 ** n]. *)
+val nan : float
+(** A special floating-point value denoting the result of an
+    undefined operation such as [0.0 /. 0.0].  Stands for ``not
+    a number''.  Any floating-point operation with [nan] as
+    argument returns [nan] as result.  As for floating-point
+    comparisons, [=], [<], [<=], [>] and [>=] return [false] and
+    [<>] returns [true] if one or both of their arguments is
+    [nan]. *)
 
-    external modf : float -> float * float = "caml_modf_float"
+(** Numeric constants *)
+
+(** The smallest positive float [x] such that [1.0 +. x <> 1.0]. *)
+val epsilon : float
+
+(** Euler? ... Euler? ... Euler? @since 2.0*)
+val e: float
+
+(** [Math.log2 e] @since 2.0 *)
+val log2e: float
+
+(** [log10 e] @since 2.0 *)
+val log10e: float
+
+(** [log 2] @since 2.0 *)
+val ln2: float
+
+(** [log 10] @since 2.0 *)
+val ln10: float
+
+(** The constant pi (3.14159...) *)
+val pi : float
+
+(** [pi /. 2.] @since 2.0 *)
+val pi2: float
+
+(** [pi /. 4.] @since 2.0 *)
+val pi4: float
+
+(** [1. /. pi] @since 2.0 *)
+val invpi: float
+
+(** [2. /. pi] @since 2.0 *)
+val invpi2: float
+
+(** [2. *. sqrt pi] @since 2.0 *)
+val sqrtpi2: float
+
+(** [sqrt 2.] @since 2.0 *)
+val sqrt2: float
+
+(** [1. /. sqrt 2.] @since 2.0 *)
+val invsqrt2: float
+
+
+(** {6 Operations on the internal representation of floating-point
+    numbers}*)
+
+external frexp : float -> float * int = "caml_frexp_float"
+(** [frexp f] returns the pair of the significant and the exponent
+    of [f].  When [f] is zero, the significant [x] and the
+    exponent [n] of [f] are equal to zero.  When [f] is non-zero,
+    they are defined by [f = x *. 2 ** n] and [0.5 <= x < 1.0]. *)
+
+external ldexp : float -> int -> float = "caml_ldexp_float"
+(** [ldexp x n] returns [x *. 2 ** n]. *)
+
+external modf : float -> float * float = "caml_modf_float"
       (** [modf f] returns the pair of the fractional and integral
 	  part of [f]. *)
 
