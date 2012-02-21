e67e94f7812721dbbcfc0403812a0dd92b1454b8
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2012-02-21 23:58:55 +0100

    char: layout + indent

--- src/batChar.ml
+++ src/batChar.ml
@@ -19,94 +19,102 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-  (*BISECT-IGNORE-BEGIN*)
-  let is_whitespace = function
-    ' ' | '\010' | '\013' | '\009' | '\026' | '\012' -> true
+(*BISECT-IGNORE-BEGIN*)
+let is_whitespace = function
+  | ' ' | '\010' | '\013' | '\009' | '\026' | '\012' -> true
   | _ -> false
 
-  let is_newline = function
-      '\010' | '\013' -> true
-    | _               -> false
+let is_newline = function
+  | '\010' | '\013' -> true
+  | _               -> false
 
-  let is_digit = function
-    | '0'..'9' -> true
-    | _ -> false
+let is_digit = function
+  | '0'..'9' -> true
+  | _ -> false
 
-  let is_uppercase c = 'A' <= c && c <= 'Z'
-  let is_lowercase c = 'a' <= c && c <= 'z'
+let is_uppercase c = 'A' <= c && c <= 'Z'
+let is_lowercase c = 'a' <= c && c <= 'z'
 
-let is_uppercase_latin1 c = is_uppercase c || ( '\192' (*À*)<= c && c <= '\214' (*Ö*) )||( '\216' (*Ø*) <= c && c <= '\221'(*Ý*) )
-let is_lowercase_latin1 c = is_lowercase c || ( '\222' (*Þ*) <= c && c <= '\246'(*ö*) )||( '\248'(*ø*) <= c && c <= '\255' (*'ÿ'*) )
-let is_latin1 c = is_uppercase_latin1 c || is_lowercase_latin1 c
+let is_uppercase_latin1 c =
+  is_uppercase c ||
+  ( '\192' (*À*)<= c && c <= '\214' (*Ö*) ) ||
+  ( '\216' (*Ø*) <= c && c <= '\221'(*Ý*) )
 
-  let is_symbol             = function
-    | '!' | '%' | '&' | '$' | '#' | '+' | '-' | '/' | ':' | '<' | '=' |
-          '>' | '?' | '@' | '\\' | '~' | '^' | '|' | '*' -> true
-    | _ -> false
+let is_lowercase_latin1 c =
+  is_lowercase c ||
+  ( '\222' (*Þ*) <= c && c <= '\246'(*ö*) ) ||
+  ( '\248'(*ø*) <= c && c <= '\255' (*'ÿ'*) )
 
-  let is_letter c =
-    is_uppercase c || is_lowercase c
+let is_latin1 c =
+  is_uppercase_latin1 c || is_lowercase_latin1 c
 
-  external unsafe_int : char-> int  = "%identity"
-  external unsafe_chr : int -> char = "%identity"
-  (*BISECT-IGNORE-END*)
+let is_symbol = function
+  | '!' | '%' | '&' | '$' | '#' | '+' | '-' | '/' | ':' | '<' | '='
+  | '>' | '?' | '@' | '\\' | '~' | '^' | '|' | '*' -> true
+  | _ -> false
 
-  let of_digit i =
-    if i >= 0 && i < 10 then
-      Char.unsafe_chr (i + Char.code '0')
-    else
-      invalid_arg "Char.of_digit"
-  (*$T of_digit
-    of_digit 6 = '6'
-    try ignore (of_digit (-2)); false with Invalid_argument _ -> true
-    try ignore (of_digit (46)); false with Invalid_argument _ -> true
-  *)
+let is_letter c =
+  is_uppercase c || is_lowercase c
 
-  let enum () =
-    BatEnum.map unsafe_chr (BatEnum.( -- ) 0 255)
-  (*$T enum
-    let e = enum () in for i = 0 to 255 do assert (Char.chr i = BatEnum.get_exn e) done; BatEnum.is_empty e
-  *)
+external unsafe_int : char-> int  = "%identity"
+external unsafe_chr : int -> char = "%identity"
+(*BISECT-IGNORE-END*)
 
-  let ( -- ) from last =
-    BatEnum.map unsafe_chr (BatEnum.( -- ) (unsafe_int from) (unsafe_int last))
-  (*$T (--)
-    let e = Char.chr 12 -- Char.chr 52 in for i = 12 to 52 do assert (Char.chr i = BatEnum.get_exn e) done; BatEnum.is_empty e
-  *)
+let of_digit i =
+  if i >= 0 && i < 10 then
+    Char.unsafe_chr (i + Char.code '0')
+  else
+    invalid_arg "Char.of_digit"
+(*$T of_digit
+  of_digit 6 = '6'
+  try ignore (of_digit (-2)); false with Invalid_argument _ -> true
+  try ignore (of_digit (46)); false with Invalid_argument _ -> true
+*)
 
-  (*BISECT-IGNORE-BEGIN*)
-  let range ?until:(last = unsafe_chr 255) from =
-      from -- last
-  (*BISECT-IGNORE-END*)
-  
+let enum () =
+  BatEnum.map unsafe_chr (BatEnum.( -- ) 0 255)
+(*$T enum
+  let e = enum () in for i = 0 to 255 do assert (Char.chr i = BatEnum.get_exn e) done; BatEnum.is_empty e
+*)
 
-  module Infix = struct
-    let (--) = (--)
-  end
+let ( -- ) from last =
+  BatEnum.map unsafe_chr (BatEnum.( -- ) (unsafe_int from) (unsafe_int last))
+(*$T (--)
+  let e = Char.chr 12 -- Char.chr 52 in for i = 12 to 52 do assert (Char.chr i = BatEnum.get_exn e) done; BatEnum.is_empty e
+*)
 
-  let print out t = BatInnerIO.write out t
-  let t_printer _paren out t =
-    BatInnerIO.write out '\'';
-    BatInnerIO.nwrite out (Char.escaped t);
-    BatInnerIO.write out '\''
-  (*$T print
-    BatIO.to_string print 'a' = "a"
-    BatIO.to_string print '\n' = "\n"
-  *)
-  (*$T t_printer
-    BatIO.string_of_t_printer t_printer '\n' = "'\\n'"
-    BatIO.string_of_t_printer t_printer '\000' = "'\\000'"
-    BatIO.string_of_t_printer t_printer 'a' = "'a'"
-  *)
+(*BISECT-IGNORE-BEGIN*)
+let range ?until:(last = unsafe_chr 255) from =
+  from -- last
+(*BISECT-IGNORE-END*)
 
-  (*BISECT-IGNORE-BEGIN*)
-  let cmp (x:char) y = Pervasives.compare x y
-  let ord (x:char) y =
-    if x > y then BatOrd.Gt
-    else if y > x then BatOrd.Lt
-    else BatOrd.Eq
-  let eq (x:char) y = x == y (* safe because int-like value *)
-  (*BISECT-IGNORE-END*)
+module Infix = struct
+  let (--) = (--)
+end
+
+let print out t = BatInnerIO.write out t
+let t_printer _paren out t =
+  BatInnerIO.write out '\'';
+  BatInnerIO.nwrite out (Char.escaped t);
+  BatInnerIO.write out '\''
+(*$T print
+  BatIO.to_string print 'a' = "a"
+  BatIO.to_string print '\n' = "\n"
+*)
+(*$T t_printer
+  BatIO.string_of_t_printer t_printer '\n' = "'\\n'"
+  BatIO.string_of_t_printer t_printer '\000' = "'\\000'"
+  BatIO.string_of_t_printer t_printer 'a' = "'a'"
+*)
+
+(*BISECT-IGNORE-BEGIN*)
+let cmp (x:char) y = Pervasives.compare x y
+let ord (x:char) y =
+  if x > y then BatOrd.Gt
+  else if y > x then BatOrd.Lt
+  else BatOrd.Eq
+let eq (x:char) y = x == y (* safe because int-like value *)
+(*BISECT-IGNORE-END*)
 
 module Incubator = struct
   module Comp = struct
