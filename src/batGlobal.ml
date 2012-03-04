eff466c437aa63ab91a153b1acb37270489cc98e
Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>
2012-03-04 19:41:22 +0100

    global: reindent

--- src/batGlobal.ml
+++ src/batGlobal.ml
@@ -23,20 +23,25 @@ exception Global_not_initialized of string
 
 type 'a t = ('a option ref * string)
 
-let empty name = ref None,name
+let empty name =
+  (ref None, name)
 
 let name = snd
 
-let set (r,_) v = r := Some v
+let set (r, _) v =
+  r := Some v
 
-let get (r,name) =
-	match !r with
-	| None -> raise (Global_not_initialized name)
-	| Some v -> v
+let get (r, name) =
+  match !r with
+  | None -> raise (Global_not_initialized name)
+  | Some v -> v
 
-let undef (r,_) = r := None
+let undef (r, _) =
+  r := None
 
-let isdef (r,_) = !r <> None
+let isdef (r, _) =
+  !r <> None
 
-let opt (r,_) = !r
+let opt (r, _) =
+  !r
 
