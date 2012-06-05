cf010320753f0e463856b53d76b288aaa8a46781
Eric Norige <thelema314@gmail.com>
2012-06-05 13:13:39 -0400

    Fix cat.ml example to compile
    Also add comment saying how to compile

--- examples/tools/cat.ml
+++ examples/tools/cat.ml
@@ -1,11 +1,14 @@
 (** Implementation of a cat-like tool: read each file whose name is
     given on the command-line and print the contents to stdout.
 
-    Usage: 
+    Compilation:
+     ocamlbuild cat.byte
+
+    Usage:
      ./cat.byte *.ml
-     
+
 *)
 
-open Batteries
+open Batteries_uni;;
 
 iter (fun x -> IO.copy (File.open_in x) stdout) (args ());;
