9da25fd44e274ac1a98aff8102f6f0d7264dd302
Eric Norige <thelema314@gmail.com>
2012-07-19 09:59:22 -0400

    Remove escape_arg_win; already implemented under win32 as Filename.quote

--- src/batSys.mli
+++ src/batSys.mli
@@ -228,7 +228,3 @@ val ocaml_version : string;;
 val files_of: string -> string BatEnum.t
 (**As {!readdir} but the results are presented as an enumeration
    of names.*)
-
-
-(**/**)
-val escape_arg_win : string -> string
