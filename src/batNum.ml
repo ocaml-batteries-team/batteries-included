b476b844b666ae0cb99c20a2d185b22f40f12ea2
Christopher Zimmermann <madroach@gmerlin.de>
2013-01-04 19:09:53 +0100

    fix BatNum.of_float
    - do conversion arithethically, not by parsing strings.
    - add appropriate test cases.

--- src/batNum.ml
+++ src/batNum.ml
@@ -45,14 +45,24 @@ module BaseNum = struct
   let succ       = succ_num
 
   let of_float f =
-    let s = Printf.sprintf "%f" f in
-      try 
-	let (prefix, suffix) = BatString.split s "."    in
-	let float_digits     = String.length suffix  in
-	let divider = pow (Int 10) (Int (String.length s - float_digits)) in
-	let dividee = Big_int (Big_int.big_int_of_string  (prefix^suffix))        in
-	  div divider dividee
-      with Not_found -> of_int (BatInt.of_float f)
+    match classify_float f with
+    | FP_normal
+    | FP_subnormal ->
+        let x,e = frexp f in
+        let n,e =
+          Big_int.big_int_of_int64 (Int64.of_float (ldexp x 52)),
+          (e-52)
+        in
+        if e >= 0 then
+          Big_int (Big_int.shift_left_big_int n e)
+        else
+          div
+            (Big_int n)
+            (Big_int Big_int.(shift_left_big_int unit_big_int ~-e))
+    | FP_zero -> zero
+    | FP_nan -> div zero zero
+    | FP_infinite ->
+        if f >= 0. then div one zero else div (neg one) zero
 end
   
 include BatNumber.MakeNumeric(BaseNum)
