8bbdca57fac2456d5852b1c117f48a61734635cc
Cedric Cellier <rixed@happyleptic.org>
2013-04-13 20:55:36 +0200

    Addition of map_lazy_default, and use lazy for lazy_default as well.

--- src/batOption.mli
+++ src/batOption.mli
@@ -75,7 +75,7 @@ val ( |? ) : 'a option -> 'a -> 'a
 
     @since 2.0 *)
 
-val default_f : (unit -> 'a) -> 'a option -> 'a
+val lazy_default : 'a Lazy.t -> 'a option -> 'a
 (** Like {!default} but the default computation is delayed until actually needed.
 
     @since NEXT_RELEASE *)
@@ -83,6 +83,11 @@ val default_f : (unit -> 'a) -> 'a option -> 'a
 val map_default : ('a -> 'b) -> 'b -> 'a option -> 'b
 (** [map_default f x (Some v)] returns [f v] and [map_default f x None]
 	returns [x]. *)
+
+val map_lazy_default : ('a -> 'b) -> 'b Lazy.t -> 'a option -> 'b
+(** Like {!map_default} but the default is only computed (once) if actually needed.
+
+    @since NEXT_RELEASE *)
 
 val is_none : 'a option -> bool
 (** [is_none None] returns [true] otherwise it returns [false]. *)
