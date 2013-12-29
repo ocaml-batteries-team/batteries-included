c93f474a4446c30ec8e252685aad62a37f6e1d0f
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-12-29 22:34:14 +0100

    tests for Seq

--- src/batSeq.ml
+++ src/batSeq.ml
@@ -146,10 +146,21 @@ let iteri f s =
   | Cons(e, s) -> f i e; iteri f (i+1) s
   in iteri f 0 s
 
+(*$T iteri
+  try iteri (fun i x -> if i<>x then raise Exit) (of_list [0;1;2;3]); true \
+  with Exit -> false
+*)
+
 let rec iter2 f s1 s2 = match s1 (), s2 () with
   | Nil, _
   | _, Nil -> ()
   | Cons (x1, s1'), Cons (x2, s2') -> f x1 x2; iter2 f s1' s2'
+
+(*$T iter2
+  let r = ref 0 in \
+    iter2 (fun i j -> r := !r + i*j) (of_list [1;2]) (of_list [3;2;1]); \
+    !r = 3 + 2*2
+ *)
 
 let rec map f s () = match s () with
   | Nil -> Nil
@@ -161,11 +172,21 @@ let mapi f s =
   | Cons(x, s) -> Cons(f i x, mapi f (i+1) s)
   in mapi f 0 s
 
+(*$T mapi
+  equal (of_list [0;0;0;0]) \
+    (mapi (fun i x -> i - x) (of_list [0;1;2;3]))
+ *)
+
 let rec map2 f s1 s2 () = match s1 (), s2 () with
   | Nil, _
   | _, Nil -> Nil
   | Cons (x1, s1'), Cons (x2, s2') ->
     Cons (f x1 x2, map2 f s1' s2')
+
+(*$T map2
+  equal (map2 (+) (of_list [1;2;3]) (of_list [3;2])) \
+    (of_list [4;4])
+ *)
 
 let rec fold_left f acc s = match s () with
   | Nil -> acc
