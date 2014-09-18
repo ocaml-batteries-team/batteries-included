29f7b783e8b9630547c3cf0c82ff74eae53226df
Christopher Zimmermann <madroach@gmerlin.de>
2014-09-18 12:47:33 +0200

    add Queue.Concrete module
    to encapsulate functions working with the concrete representation of
    queues. Use Queue.clear and Queue.is_empty instead of comparing /
    assigning (Obj.magic None).
    Also add comments to and simplify Queue.filter.

--- src/batQueue.ml
+++ src/batQueue.ml
@@ -20,51 +20,94 @@
  *)
 
 
+module Concrete = struct
+(* Explanation from OCaml 4.02 source:
+
+   A queue is a reference to either nothing or some cell of a cyclic
+   list. By convention, that cell is to be viewed as the last cell in
+   the queue. The first cell in the queue is then found in constant
+   time: it is the next cell in the cyclic list. The queue's length is
+   also recorded, so as to make [length] a constant-time operation.
+
+   The [tail] field should really be of type ['a cell option], but
+   then it would be [None] when [length] is 0 and [Some] otherwise,
+   leading to redundant memory allocation and accesses. We avoid this
+   overhead by filling [tail] with a dummy value when [length] is 0.
+   Of course, this requires bending the type system's arm slightly,
+   because it does not have dependent sums.
+   The dummy value used by the stdlib is (Obj.magic None). *)
+
+  type 'a cell = {
+    content: 'a;
+    mutable next: 'a cell
+  }
+  and 'a t = {
+    mutable length: int;
+    mutable tail: 'a cell
+  }
+
+  external of_abstr : 'a Queue.t -> 'a t = "%identity"
+  external to_abstr : 'a t -> 'a Queue.t = "%identity"
+
+  let filter f ({tail} as queue) =
+    if not (Queue.is_empty (to_abstr queue)) then
+      let rec filter'
+          ({ next = { content; next} as current } as prev)
+        =
+        if f content
+        then
+          (* Keep cell. Recursion to next cell unless we reached the tail *)
+          (if current != tail then filter' current)
+        else begin
+          (* Remove cell. *)
+          if current != tail
+          then begin
+            (* Easy case. We are not removing the tail cell. *)
+            prev.next <- next;
+            queue.length <- queue.length - 1;
+            (* Recursion with the same cell,
+             * because it is now pointing beyond current. *)
+            filter' prev
+          end
+          else begin
+            (* Removing the tail cell *)
+            if prev == current
+            (* Tail cell is the last cell. Just clear the queue. *)
+            then begin
+              Queue.clear (to_abstr queue)
+            end
+            else begin
+              (* Tail cell is not the last cell.
+               * prev is the new tail. *)
+              prev.next <- next;
+              queue.length <- queue.length - 1;
+              queue.tail <- prev;
+            end
+          end
+        end
+      in
+      filter' tail
+end
+
 include Queue
 
-type 'a cell = {
-  content: 'a;
-  mutable next: 'a cell
-}
-
-type 'a queue = {
-  mutable length: int;
-  mutable tail: 'a cell
-}
-
-let filter f q =
-  let ({length; tail} as queue) = Obj.magic q in
-  if tail <> Obj.magic None then
-    let rec filter'
-        ({ next = { content; next} as current } as prev)
-      =
-      if f content
-      then
-        (if current != tail then filter' current)
-      else begin
-        queue.length <- queue.length - 1;
-        if prev == current
-        then
-          queue.tail <- Obj.magic None
-        else begin
-          prev.next <- next;
-          if current == tail
-          then
-            queue.tail <- prev
-          else
-            filter' prev
-        end
-      end
-    in
-    filter' tail
-(*$T filter
-  let q = Queue.create () in \
-  for i = 1 to 5 do Queue.push i q; done; \
-  filter (fun a -> List.mem a [1;3;5]) q; \
-  BatList.of_enum (enum q) = [2;5]
-*)
-
 type 'a enumerable = 'a t
+
+let filter f q = Concrete.(filter f (of_abstr q))
+(*$T filter
+  let q1 = Queue.create () in \
+  for i = 1 to 5 do Queue.push i q1; done; \
+  let q2,q3 = Queue.copy q1, Queue.copy q1 in
+  filter (fun a -> List.mem a [2;4]) q1; \
+  filter (fun a -> List.mem a [3]) q2; \
+  filter (fun a -> List.mem a []) q3; \
+  length q1 = 2 && \
+  length q2 = 1 && \
+  length q3 = 0 && \
+  BatList.of_enum (enum q1) = [2;4] && \
+  BatList.of_enum (enum q2) = [3] && \
+  BatList.of_enum (enum q3) = []
+*)
 
 let of_enum e =
   let q = create () in
