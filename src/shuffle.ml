539d63dde5ef83f381c5ddf39f240d4fb565a9bb
Francois BERENGER <ligand@free.fr>
2017-01-16 11:37:41 +0100

    added src/shuffle.ml

---
+++ src/shuffle.ml
@@ -0,0 +1,16 @@
+let array_shuffle rand_state a =
+  (match rand_state with
+   | Some s -> Random.set_state s
+   | None -> Random.self_init ());
+  for n = Array.length a - 1 downto 1 do
+    let k = Random.int (n + 1) in
+    if k <> n then
+      let buf = Array.unsafe_get a n in
+      Array.unsafe_set a n (Array.unsafe_get a k);
+      Array.unsafe_set a k buf
+  done;
+  a
+
+let list_shuffle rand_state l =
+  let a = array_shuffle rand_state (Array.of_list l) in
+  Array.to_list a
