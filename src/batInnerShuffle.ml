1ac876d2e401a0defcf424af76c090b5c6991a38
Francois BERENGER <ligand@free.fr>
2017-02-07 16:02:46 -0600

    added in-place array shuffle shared by BatArray, BatList and BatRandom
    heavily inspired by the in-place array shuffle that was found
    in BatRandom before

---
+++ src/batInnerShuffle.ml
@@ -0,0 +1,10 @@
+
+let array_shuffle ?state:(s = Random.get_state ()) a =
+  for n = Array.length a - 1 downto 1 do
+    let k = Random.State.int s (n + 1) in
+    if k <> n then
+      let buf = Array.unsafe_get a n in
+      Array.unsafe_set a n (Array.unsafe_get a k);
+      Array.unsafe_set a k buf
+  done;
+  a
