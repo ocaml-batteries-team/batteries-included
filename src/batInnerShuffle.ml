8ecfd5fda89a5b9563d314755c74bfc34dc6315e
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-02-10 17:24:28 +0100

    fix the handling of optional random-generator state in BatInnerShuffle
    The behavior we want for randomness-manipulating function with an
    optional parameter is that:
    - if a state is passed, it is used (and mutated) for random generation
    - if no state is passed, the default/global random state is used
      (and mutated) for random generation
    
    It might seem that a parameter with default value
    
      ?(state = Random.get_state ())
    
    would suffice to get this behavior, but in fact Random.get_state ()
    performs a copy of the default/global random state, instead of just
    returning it, so the default/global state is not mutated by calls to
    shuffles (they will not be truly random).
    
    The current implementation avoids this pitfall by calling the
    global/default Random.int function directly.

--- src/batInnerShuffle.ml
+++ src/batInnerShuffle.ml
@@ -1,10 +1,13 @@
-
-let array_shuffle ?state:(s = Random.get_state ()) a =
+let array_shuffle ?state a =
+  let random_int state n = match state with
+    | None -> Random.int n
+    | Some s -> Random.State.int s n in
   for n = Array.length a - 1 downto 1 do
-    let k = Random.State.int s (n + 1) in
-    if k <> n then
+    let k = random_int state (n + 1) in
+    if k <> n then begin
       let buf = Array.unsafe_get a n in
       Array.unsafe_set a n (Array.unsafe_get a k);
       Array.unsafe_set a k buf
+    end
   done;
   a
