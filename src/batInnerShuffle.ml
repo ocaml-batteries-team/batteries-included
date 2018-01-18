6c2ea42eb3eea41e9d6c23ab59cb7ac6383c1725
Francois BERENGER <ligand@free.fr>
2018-01-18 11:26:40 +0900

    simpler is better (and probably more efficient)

--- src/batInnerShuffle.ml
+++ src/batInnerShuffle.ml
@@ -1,9 +1,10 @@
+
 let array_shuffle ?state a =
-  let random_int state n = match state with
-    | None -> Random.int n
-    | Some s -> Random.State.int s n in
+  let random_int = match state with
+    | None -> Random.int
+    | Some s -> Random.State.int s in
   for n = Array.length a - 1 downto 1 do
-    let k = random_int state (n + 1) in
+    let k = random_int (n + 1) in
     if k <> n then begin
       let buf = Array.unsafe_get a n in
       Array.unsafe_set a n (Array.unsafe_get a k);
