90b426fa6a224ae397c68050ec0877059d292822
Gregory Nisbet <gregory.nisbet@gmail.com>
2018-08-29 19:27:12 -0700

    use OUnit.assert_bool not assert in test

--- testsuite/test_option.ml
+++ testsuite/test_option.ml
@@ -1,5 +1,4 @@
 open OUnit
-open BatPervasives
 open BatOption
 
 let fail_eq x y = failwith "should not be called"
@@ -8,18 +7,18 @@ let poly_eq = Pervasives.(=)
 
 let tests = "Option tests" >::: [
   "None = None" >:: begin function () ->
-    assert (equal ~eq:fail_eq None None)
+    assert_bool "None should equal itself" (equal ~eq:fail_eq None None)
   end;
   "None <> Some x" >:: begin function () ->
-    assert (not (equal ~eq:fail_eq None (Some 4)))
+    assert_bool "None and Some shouldn't amtch" (not (equal ~eq:fail_eq None (Some 4)))
   end;
   "Some x <> None" >:: begin function () ->
-    assert (not (equal ~eq:fail_eq (Some 4) None))
+    assert_bool "Some and None shouldn't match" (not (equal ~eq:fail_eq (Some 4) None))
   end;
   "Some 4 = Some 4" >:: begin function () ->
-    assert (equal ~eq:poly_eq (Some 4) (Some 4))
+    assert_bool "Some 4 should match itself" (equal ~eq:poly_eq (Some 4) (Some 4))
   end;
   "Some 4 = Some 5" >:: begin function () ->
-    assert (not (equal ~eq:poly_eq (Some 4) (Some 5)))
+    assert_bool "Some 4 shouldn't match Some 5" (not (equal ~eq:poly_eq (Some 4) (Some 5)))
   end;
 ]
