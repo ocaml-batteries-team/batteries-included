03bcda856a3bfc1f76f559b076f8db20bff6b495
Cedric Cellier <rixed@happyleptic.org>
2019-10-02 09:10:45 +0200

    Make BatResult a superset of OCaml new Result module. (#922)
    Breaks compatibility with previous batteries major versions by renaming BatResult.Bad into
    OCaml stdlib's new Result.Error.
    
    Closes #839

--- src/batResult.ml
+++ src/batResult.ml
@@ -1,40 +1,81 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                 OCaml                                  *)
+(*                                                                        *)
+(*                         The OCaml programmers                          *)
+(*                                                                        *)
+(*   Copyright 2018 Institut National de Recherche en Informatique et     *)
+(*     en Automatique.                                                    *)
+(*                                                                        *)
+(*   All rights reserved.  This file is distributed under the terms of    *)
+(*   the GNU Lesser General Public License version 2.1, with the          *)
+(*   special exception on linking described in the file LICENSE.          *)
+(*                                                                        *)
+(**************************************************************************)
 
-type ('a, 'b) t = ('a, 'b) BatPervasives.result =
+type ('a, 'e) t = ('a, 'e) BatPervasives.result =
   | Ok  of 'a
-  | Bad of 'b
+  | Error of 'e
 
-let catch f x = try Ok (f x) with e -> Bad e
-let catch2 f x y = try Ok (f x y) with e -> Bad e
-let catch3 f x y z = try Ok (f x y z) with e -> Bad e
+let ok v = Ok v
+let error e = Error e
+let value r ~default = match r with Ok v -> v | Error _ -> default
+let get_ok = function Ok v -> v | Error _ -> invalid_arg "result is Error _"
+let get_error = function Error e -> e | Ok _ -> invalid_arg "result is Ok _"
+let bind r f = match r with Ok v -> f v | Error _ as e -> e
+let join = function Ok r -> r | Error _ as e -> e
+let map_error f = function Error e -> Error (f e) | Ok _ as v -> v
+let fold ~ok ~error = function Ok v -> ok v | Error e -> error e
+let iter f = function Ok v -> f v | Error _ -> ()
+let iter_error f = function Error e -> f e | Ok _ -> ()
+let is_error = function Error _ -> true | Ok _ -> false
+
+let equal ~ok ~error r0 r1 = match r0, r1 with
+| Ok v0, Ok v1 -> ok v0 v1
+| Error e0, Error e1 -> error e0 e1
+| _, _ -> false
+
+let compare ~ok ~error r0 r1 = match r0, r1 with
+| Ok v0, Ok v1 -> ok v0 v1
+| Error e0, Error e1 -> error e0 e1
+| Ok _, Error _ -> -1
+| Error _, Ok _ -> 1
+
+let to_list = function Ok v -> [v] | Error _ -> []
+let to_seq = function Ok v -> BatSeq.(cons v nil) | Error _ -> BatSeq.nil
+
+let catch f x = try Ok (f x) with e -> Error e
+let catch2 f x y = try Ok (f x y) with e -> Error e
+let catch3 f x y z = try Ok (f x y z) with e -> Error e
 
 let of_option = function
   | Some x -> Ok x
-  | None   -> Bad ()
+  | None   -> Error ()
 
 let to_option = function
   | Ok x   -> Some x
-  | Bad _-> None
+  | Error _-> None
 
 let default def = function
   | Ok x  -> x
-  | Bad _ -> def
+  | Error _ -> def
 
 let map f = function
-  | Bad e -> Bad e
+  | Error e -> Error e
   | Ok v  -> Ok (f v)
 (*$T map
-  map succ (Bad (-1)) = (Bad (-1))
-  map succ (Bad 0) = (Bad 0)
+  map succ (Error (-1)) = (Error (-1))
+  map succ (Error 0) = (Error 0)
   map succ (Ok 3) = (Ok 4)
 *)
 
 let map_both f g = function
-  | Bad e -> Bad (g e)
+  | Error e -> Error (g e)
   | Ok v  -> Ok (f v)
 (*$T map_both
-  map_both succ pred (Bad (-1)) = (Bad (-2))
-  map_both succ pred (Bad 0) = (Bad (-1))
-  map_both succ pred (Bad 1) = (Bad 0)
+  map_both succ pred (Error (-1)) = (Error (-2))
+  map_both succ pred (Error 0) = (Error (-1))
+  map_both succ pred (Error 1) = (Error 0)
   map_both succ pred (Ok (-1)) = (Ok 0)
   map_both succ pred (Ok 0) = (Ok 1)
   map_both succ pred (Ok 1) = (Ok 2)
@@ -42,25 +83,25 @@ let map_both f g = function
 
 let map_default def f = function
   | Ok x -> f x
-  | Bad _ -> def
+  | Error _ -> def
 
-let is_ok = function Ok _ -> true | Bad _ -> false
+let is_ok = function Ok _ -> true | Error _ -> false
 
-let is_bad = function Bad _ -> true | Ok _ -> false
+let is_bad = function Error _ -> true | Ok _ -> false
 
-let is_exn e = function Bad exn -> exn = e | Ok _ -> false
+let is_exn e = function Error exn -> exn = e | Ok _ -> false
 
-let get = function Ok x -> x | Bad e -> raise e
+let get = function Ok x -> x | Error e -> raise e
 
 let print print_val oc = function
   | Ok x -> BatPrintf.fprintf oc "Ok(%a)" print_val x
-  | Bad e -> BatPrintf.fprintf oc "Bad(%a)" BatPrintexc.print e
+  | Error e -> BatPrintf.fprintf oc "Error(%a)" BatPrintexc.print e
 
 
 module Monad = struct
   let bind m k = match m with
     | Ok  x      -> k x
-    | Bad _ as e -> e
+    | Error _ as e -> e
 
   let return x = Ok x
 
