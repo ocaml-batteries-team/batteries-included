7f853c8d7125d020433e8f48a31b1293185a7825
jakob krainz <jakob@kringel>
2021-01-29 22:14:47 +0100

    some explanation on splay trees

--- src/batSplay.mli
+++ src/batSplay.mli
@@ -18,7 +18,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *)
 
-(** Maps and sets based on splay trees *)
+(** Maps over ordered types based on splay trees.
+
+    Splay trees are ordered binary trees that have the 
+    most recently used element as the root of the tree.
+    If another element is accessed (even read-only), 
+    the tree will be rearranged internally.
+    
+    Not threadsafe; even read-only functions will rearrange
+    the tree, even though its contents will remain unchanged.
+ *)
 
 module Map (Ord : BatInterfaces.OrderedType)
   : sig
