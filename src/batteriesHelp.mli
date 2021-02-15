bcd581730240c07b29e7061c38787c99670bfb4d
Francois Berenger <unixjunkie@sdf.org>
2021-02-15 13:52:43 +0900

    add back some files that were moved

---
+++ src/batteriesHelp.mli
@@ -0,0 +1,58 @@
+(*
+ * Batteries_help - Calling the help system from the toplevel
+ * Copyright (C) 2008 David Teller, LIFO, Universite d'Orleans
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version,
+ * with the special exception on linking described in file LICENSE.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
+
+(** Tools for reading the documentation from the toplevel
+
+    All these tools are invoked automatically by the Batteries
+    Toplevel.  They are provided here if you wish to integrate them
+    into your own toplevel.
+
+    @author David Teller
+*)
+
+type kinds =
+  | Values
+  | Types
+  | Topics
+  | Modules
+  | Exns
+  | Modtypes
+  | Classes
+  | Methods
+  | Attributes
+  | Objtypes
+
+val init : unit -> unit
+(** Proceed to initialization.
+
+    This function loads the primary help files and registers the
+    toplevel directives.
+
+    If you integrate the on-line help system into your toplevel, you
+    must call this function before any of the other functions of this
+    module. *)
+
+val help : unit -> unit
+(** [help ()] opens the tutorial.*)
+
+val man           : string -> unit
+  (** [man "something"] opens the help about subject ["something"].
+  *)
+
---
+++ src/batteries_compattest.ml
@@ -0,0 +1,134 @@
+open Batteries
+
+module Stdlib_verifications = struct
+  (* This module asserts that all the BatFoo modules are actually
+     extensions of stdlib modules, and that no functionality is lost. *)
+  module Array_t = (Array : module type of Legacy.Array)
+  module Buffer_t =
+    (Buffer: sig
+       include module type of Legacy.Buffer
+       val add_channel : t -> BatInnerIO.input -> int -> unit
+       val output_buffer : t -> string BatInnerIO.output
+     end)
+  module Bytes = (Bytes : module type of Legacy.Bytes)
+  module Char_t = (Char: module type of Legacy.Char)
+  module Complex_t = (Complex : module type of Legacy.Complex)
+  module Digest =
+    (Digest: sig
+       include module type of Legacy.Digest
+       val channel : BatIO.input -> int -> Digest.t
+       val output : 'a BatIO.output -> t -> unit
+       val input : BatIO.input -> Digest.t
+     end)
+  (*  module Format = (Format: module type of Legacy.Format)*)
+  module Gc =
+    (Gc: sig
+       include module type of Legacy.Gc
+       val print_stat : 'a BatInnerIO.output -> unit
+     end)
+  module Genlex = (Genlex : module type of Legacy.Genlex)
+  (*  module Hashtbl = (Hashtbl: module type of Legacy.Hashtbl)*)
+  module Int32 = (Int32: module type of Legacy.Int32)
+  module Int64 = (Int64: module type of Legacy.Int64)
+  module Lexing =
+    (Lexing: sig
+       include module type of Legacy.Lexing
+       val from_channel : BatIO.input -> Lexing.lexbuf
+     end)
+  module List =
+    (List: sig
+       include module type of Legacy.List
+       val find_map : ('a -> 'b option) -> 'a list -> 'b
+     end)
+ module Seq = (Seq : module type of Legacy.Seq)
+  module Marshal =
+    (Marshal: sig
+       include module type of Legacy.Marshal
+       val to_channel : _ BatIO.output -> 'b -> extern_flags list -> unit
+       val from_channel : BatIO.input -> 'a
+     end)
+  module Nativeint = (Nativeint: module type of Legacy.Nativeint)
+  module Oo = (Oo : module type of Legacy.Oo)
+  module Printexc =
+    (Printexc: sig
+       include module type of Legacy.Printexc
+       val print : 'a BatInnerIO.output -> exn -> unit
+       val print_backtrace : 'a BatInnerIO.output -> unit
+     end)
+  (*  module Printf = (Printf: module type of Legacy.Printf)*)
+  module Queue = (Queue: module type of Legacy.Queue)
+  module Random = (Random: module type of Legacy.Random)
+ module Result = (Result: module type of Legacy.Result)
+  (*  module Scanf = (Scanf : module type of Legacy.Scanf)*)
+  (* FAILS BECAUSE OF Stack.Empty not being present because
+     module Stack = (Stack : module type of Legacy.Stack)
+  *)
+  module Stream = (Stream : module type of Legacy.Stream)
+  module String = (String : module type of Legacy.String)
+  module Sys = (Sys : module type of Legacy.Sys)
+  module Unix =
+    (Unix: sig
+       include module type of Legacy.Unix
+       val in_channel_of_descr : Unix.file_descr -> BatInnerIO.input
+       val out_channel_of_descr : Unix.file_descr -> unit BatInnerIO.output
+       val descr_of_in_channel : BatInnerIO.input -> Unix.file_descr
+       val descr_of_out_channel : unit BatInnerIO.output -> Unix.file_descr
+       val open_process_in :
+         ?autoclose:bool -> ?cleanup:bool -> string -> BatInnerIO.input
+       val open_process_out :
+         ?cleanup:bool -> string -> unit BatInnerIO.output
+       val open_process :
+         ?autoclose:bool ->
+         ?cleanup:bool ->
+         string -> BatInnerIO.input * unit BatInnerIO.output
+       val open_process_full :
+         ?autoclose:bool ->
+         ?cleanup:bool ->
+         string ->
+         string array ->
+         BatInnerIO.input * unit BatInnerIO.output * BatInnerIO.input
+       val close_process_in : BatInnerIO.input -> Unix.process_status
+       val close_process_out :
+         unit BatInnerIO.output -> Unix.process_status
+       val close_process :
+         BatInnerIO.input * unit BatInnerIO.output -> Unix.process_status
+       val close_process_full :
+         BatInnerIO.input * unit BatInnerIO.output * BatInnerIO.input ->
+         Unix.process_status
+       val open_connection :
+         ?autoclose:bool ->
+         Unix.sockaddr -> BatInnerIO.input * unit BatInnerIO.output
+       val shutdown_connection : BatInnerIO.input -> unit
+       val establish_server :
+         ?autoclose:bool ->
+         ?cleanup:bool ->
+         (BatInnerIO.input -> unit BatInnerIO.output -> unit) ->
+         Unix.sockaddr -> unit
+     end)
+  module Big_int = (Big_int : module type of Legacy.Big_int)
+  (* FIXME: This does not pass for some reason:
+  module Bigarray = (Bigarray : module type of Legacy.Bigarray)*)
+   
+  (* test compatibility of BatMap.S with Legacy.Map.S *)
+  let sort_map (type s) (module Map : Legacy.Map.S with type key = s) l =
+      Map.bindings (List.fold_right (fun x m -> Map.add x x m) l Map.empty)
+  module IntMap = struct
+     include BatMap.Int
+     let update = update_stdlib
+  end
+  let _ = assert ([1,1;2,2;3,3;] = (sort_map (module IntMap) [3; 1; 2;]))
+  (* test compat of BatSplay.S with Legacy.Map.S *)
+  module IntSplayMap = struct
+     include BatSplay.Map (BatInt)
+     let update = update_stdlib
+  end
+  let _ = assert ([1,1;2,2;3,3;] = (sort_map (module IntSplayMap) [3; 1; 2;]))
+ 
+  (* test compatibility of BatSet.S with Legacy.Set.S *)
+  let sort (type s) (module Set : Legacy.Set.S with type elt = s) l =
+      Set.elements (List.fold_right Set.add l Set.empty)
+  module IntSet = struct
+      include BatSet.Int
+  end 
+  let _ = assert ([1;2;3] = (sort (module IntSet) [3; 1; 2;]))
+end
