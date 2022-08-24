cadf1f1796b876d47c88a5c54ed6e29b4d2fac55
Francois Berenger <unixjunkie@sdf.org>
2022-08-24 16:41:03 +0900

    BatGenLex: String.lowercase -> String.lowercase_ascii for ocaml5 compat

--- src/batGenlex.ml
+++ src/batGenlex.ml
@@ -320,7 +320,7 @@ struct
 
     let adapt_case =
       if case_sensitive then identity
-      else BatString.lowercase
+      else BatString.lowercase_ascii
 
     let string_compare =
       if case_sensitive then String.compare
@@ -511,7 +511,7 @@ struct
           if is_reserved x then (Kwd x)
           else                  (Ident x)
       else
-        fun x -> let x = BatString.lowercase x in
+        fun x -> let x = BatString.lowercase_ascii x in
           if is_reserved x then (Kwd x)
           else                  (Ident x)
 
